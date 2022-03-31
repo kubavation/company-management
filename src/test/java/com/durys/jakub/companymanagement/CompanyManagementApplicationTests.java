b450aaf965bea2a16189f6263341cf6d23f23dfa
kubavation <kubavation@wp.pl>
2022-03-31 20:14:27 +0200

    init

---
+++ src/test/java/com/durys/jakub/companymanagement/CompanyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.companymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
