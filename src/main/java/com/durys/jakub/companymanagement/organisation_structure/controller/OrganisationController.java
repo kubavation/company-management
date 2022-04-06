b386a35c9febeb3199f4b923e83528f244b66840
kubavation <kubavation@wp.pl>
2022-04-06 17:28:44 +0200

    fixes

--- src/main/java/com/durys/jakub/companymanagement/organisation_structure/controller/OrganisationController.java
+++ src/main/java/com/durys/jakub/companymanagement/organisation_structure/controller/OrganisationController.java
@@ -11,4 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrganisationController {
 
     private final OrganisationService organisationService;
+    
+//    @GetMapping
+//    public List<>
 }
--- src/main/java/com/durys/jakub/companymanagement/shared/exception/EntityNotFoundException.java
+++ src/main/java/com/durys/jakub/companymanagement/shared/exception/EntityNotFoundException.java
@@ -2,9 +2,9 @@ package com.durys.jakub.companymanagement.shared.exception;
 
 public class EntityNotFoundException extends RuntimeException {
 
-    private static final String message = "Entity not found (class: %s, id: %s)";
+    private static final String MSG = "Entity not found (class: %s, id: %s)";
 
     public EntityNotFoundException(Class<?> clazz, Object primaryKey) {
-        super(String.format(message, clazz.getSimpleName(), primaryKey));
+        super(String.format(MSG, clazz.getSimpleName(), primaryKey));
     }
 }
