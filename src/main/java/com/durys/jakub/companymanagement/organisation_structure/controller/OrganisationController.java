ee35d94adbb815a78908512f2d7c50e68e2bd5e4
kubavation <kubavation@wp.pl>
2022-04-13 20:25:21 +0200

    findByParentId method

--- src/main/java/com/durys/jakub/companymanagement/organisation_structure/controller/OrganisationController.java
+++ src/main/java/com/durys/jakub/companymanagement/organisation_structure/controller/OrganisationController.java
@@ -4,15 +4,14 @@ import com.durys.jakub.companymanagement.organisation_structure.converter.Organi
 import com.durys.jakub.companymanagement.organisation_structure.model.dto.OrganisationDTO;
 import com.durys.jakub.companymanagement.organisation_structure.service.OrganisationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/organisations")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class OrganisationController {
 
     private final OrganisationService organisationService;
@@ -20,8 +19,14 @@ public class OrganisationController {
     private final OrganisationStructureMapper organisationMapper;
     
     @GetMapping("/level/{level}")
-    public List<OrganisationDTO> getOrganisationStructureByLevel(Long level) {
+    public List<OrganisationDTO> getOrganisationStructureByLevel(@PathVariable Long level) {
         return organisationMapper.toDTO(
                 organisationService.findAllByLevel(level));
+    }
+
+    @GetMapping("/parent/{parentId}")
+    public List<OrganisationDTO> getOrganisationStructureByParentId(@PathVariable Long parentId) {
+        return organisationMapper.toDTO(
+                organisationService.findAllByParentId(parentId));
     }
 }
