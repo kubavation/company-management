49d76d203d553fd9372656babdd493438d3b2a4e
kubavation <kubavation@wp.pl>
2022-05-23 21:52:05 +0200

    safeDeleteRepository

--- src/main/java/com/durys/jakub/companymanagement/request/personal_request/repository/PersonalRequestTypeRepository.java
+++ src/main/java/com/durys/jakub/companymanagement/request/personal_request/repository/PersonalRequestTypeRepository.java
@@ -1,9 +1,10 @@
 package com.durys.jakub.companymanagement.request.personal_request.repository;
 
 import com.durys.jakub.companymanagement.request.personal_request.model.entity.dict.PersonalRequestType;
+import com.durys.jakub.companymanagement.shared.repository.SafeDeleteRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonalRequestTypeRepository extends JpaRepository<PersonalRequestType, Long> {
+public interface PersonalRequestTypeRepository extends SafeDeleteRepository<PersonalRequestType, Long> {
 }
--- src/main/java/com/durys/jakub/companymanagement/shared/repository/SafeDeleteRepository.java
+++ src/main/java/com/durys/jakub/companymanagement/shared/repository/SafeDeleteRepository.java
@@ -1,5 +1,6 @@
 package com.durys.jakub.companymanagement.shared.repository;
 
+import com.durys.jakub.companymanagement.shared.entity.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,10 +10,10 @@ import org.springframework.lang.NonNull;
 import java.io.Serializable;
 
 @NoRepositoryBean
-public interface SafeDeleteRepository<T,ID extends Serializable> extends JpaRepository<T,ID> {
+public interface SafeDeleteRepository<T, ID extends Serializable> extends JpaRepository<T,ID> {
 
     @Override
     @Modifying
-    @Query(value = "UPDATE #{#entityName e} set e.status = 'H' where e.id = :entity.id ")
+    @Query(value = "UPDATE #{#entityName} x set x.status = 'H' where x.id = :#{#entity.id}")
     void delete(@NonNull T entity);
 }
