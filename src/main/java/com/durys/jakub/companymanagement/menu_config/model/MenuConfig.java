eb6698e83e8ce76d1eb9161b47d7419394f3b8fe
kubavation <kubavation@wp.pl>
2022-08-08 19:43:48 +0200

    icon property in menu config

--- src/main/java/com/durys/jakub/companymanagement/menu_config/model/MenuConfig.java
+++ src/main/java/com/durys/jakub/companymanagement/menu_config/model/MenuConfig.java
@@ -3,10 +3,14 @@ package com.durys.jakub.companymanagement.menu_config.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @NoArgsConstructor
 @Data
 public class MenuConfig {
 
     private String url;
     private String name;
+    private Set<MenuConfig> children;
+    private String icon;
 }
