321a2618f15a234249018fea56a0a6239302d8a7
jakub.durys <duryskuba@wp.pl>
2022-09-27 20:16:41 +0200

    first specifications

--- src/main/java/com/durys/jakub/companymanagement/request/leave_request/util/LeaveRequestFilterUtil.java
+++ src/main/java/com/durys/jakub/companymanagement/request/leave_request/util/LeaveRequestFilterUtil.java
@@ -1,16 +1,24 @@
 package com.durys.jakub.companymanagement.request.leave_request.util;
 
 import com.durys.jakub.companymanagement.request.leave_request.model.entity.LeaveRequest;
+import com.durys.jakub.companymanagement.request.leave_request.model.entity.LeaveRequest_;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LeaveRequestFilterUtil {
 
+    //https://reflectoring.io/spring-data-specifications/
+
     public static Specification<LeaveRequest> betweenDates(LocalDate dateFrom, LocalDate dateTo) {
-        return (root, query, cb) -> null;
+        return (root, query, cb) -> cb.between(root.get(LeaveRequest_.DATE), dateFrom, dateTo);
+    }
+
+    public static Specification<LeaveRequest> withTypes(List<String> types) {
+        return (root, query, cb) -> cb.in(root.get(LeaveRequest_.TYPE)).value(types);
     }
 }
