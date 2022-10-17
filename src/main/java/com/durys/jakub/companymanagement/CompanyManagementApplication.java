63733c3396ec15625e5003a32e25551903a6c3c0
jakub.durys <duryskuba@wp.pl>
2022-10-17 17:47:37 +0200

    application.properties fix

--- src/main/java/com/durys/jakub/companymanagement/CompanyManagementApplication.java
+++ src/main/java/com/durys/jakub/companymanagement/CompanyManagementApplication.java
@@ -2,8 +2,10 @@ package com.durys.jakub.companymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CompanyManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/durys/jakub/companymanagement/employee/controller/EmployeeController.java
+++ src/main/java/com/durys/jakub/companymanagement/employee/controller/EmployeeController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
