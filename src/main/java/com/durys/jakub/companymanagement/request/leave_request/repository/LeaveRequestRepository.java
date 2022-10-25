48df123e71cc6a64979e12d5556287ae82325f7c
jakub.durys <duryskuba@wp.pl>
2022-10-25 18:19:43 +0200

    changes in leaveRequest queries

--- src/main/java/com/durys/jakub/companymanagement/request/leave_request/repository/LeaveRequestRepository.java
+++ src/main/java/com/durys/jakub/companymanagement/request/leave_request/repository/LeaveRequestRepository.java
@@ -4,6 +4,7 @@ import com.durys.jakub.companymanagement.request.leave_request.model.entity.Leav
 import com.durys.jakub.companymanagement.request.leave_request.model.enums.LeaveRequestType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,9 @@ import java.util.List;
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long>, JpaSpecificationExecutor<LeaveRequest> {
 
+    @Query("FROM LeaveRequest l WHERE l.employee.id = :employeeId and l.status = 'A' order by l.dateFrom desc")
     List<LeaveRequest> findAllByEmployeeId(Long employeeId);
+    @Query("FROM LeaveRequest l WHERE l.employee.id = :employeeId and l.status = 'A' and l.type = :type" +
+            " order by l.dateFrom desc")
     List<LeaveRequest> findAllByEmployeeIdAndType(Long employeeId, LeaveRequestType type);
 }
