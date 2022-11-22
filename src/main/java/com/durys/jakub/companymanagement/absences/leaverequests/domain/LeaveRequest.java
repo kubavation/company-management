fe30cd500c4cfe1288d71f1dd7cfd2be72584555
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-22 20:40:36 +0100

    fixes

---
+++ src/main/java/com/durys/jakub/companymanagement/absences/leaverequests/domain/LeaveRequest.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.companymanagement.absences.leaverequests.domain;
+
+class LeaveRequest {
+
+    private LeaveRequestId requestId;
+}
--- src/main/java/com/durys/jakub/companymanagement/leaveRequestDDD/domain/LeaveRequest.java
+++
@@ -1,6 +0,0 @@
-package com.durys.jakub.companymanagement.leaveRequestDDD.domain;
-
-class LeaveRequest {
-
-    private LeaveRequestId leaveRequestId;
-}
