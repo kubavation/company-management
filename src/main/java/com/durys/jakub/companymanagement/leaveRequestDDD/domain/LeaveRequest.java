5798425a680525371fb43e597550b1c6e91e0b9f
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-22 16:45:52 +0100

    resource server config

---
+++ src/main/java/com/durys/jakub/companymanagement/leaveRequestDDD/domain/LeaveRequest.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.companymanagement.leaveRequestDDD.domain;
+
+class LeaveRequest {
+
+    private LeaveRequestId leaveRequestId;
+}
--- src/main/java/com/durys/jakub/companymanagement/menuconfig/controller/MenuConfigController.java
+++ src/main/java/com/durys/jakub/companymanagement/menuconfig/controller/MenuConfigController.java
@@ -1,9 +1,7 @@
-package com.durys.jakub.companymanagement.menu_config.controller;
+package com.durys.jakub.companymanagement.menuconfig.controller;
 
-import com.durys.jakub.companymanagement.menu_config.model.MenuConfig;
-import com.durys.jakub.companymanagement.menu_config.service.MenuConfigService;
+import com.durys.jakub.companymanagement.menuconfig.model.MenuConfig;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +13,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MenuConfigController {
 
-    private final MenuConfigService menuConfigService;
-    @GetMapping
-    public List<MenuConfig> getMenuConfig() {
-        return menuConfigService.getMenuConfig();
-    }
+    //private final MenuConfigService menuConfigService;
+//    @GetMapping
+//    public List<MenuConfig> getMenuConfig() {
+//        return menuConfigService.getMenuConfig();
+//    }
 }
