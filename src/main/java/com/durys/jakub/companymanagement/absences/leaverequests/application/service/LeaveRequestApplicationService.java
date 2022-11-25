ef8051c550ac2c96cd2a455f50930835c3334656
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-25 20:14:07 +0100

    init of leaveRequestApplicationService

---
+++ src/main/java/com/durys/jakub/companymanagement/absences/leaverequests/application/service/LeaveRequestApplicationService.java
@@ -0,0 +1,7 @@
+package com.durys.jakub.companymanagement.absences.leaverequests.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeaveRequestApplicationService {
+}
