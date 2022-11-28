2d67a27f139a7951b5155ec4f23e2ba3c92f7b29
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-28 20:07:13 +0100

    acceptan entity

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/Acceptant.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/Acceptant.java
@@ -1,2 +1,16 @@
-package com.durys.jakub.companymanagement.domain.absences.leaverequests;public class Acceptant {
+package com.durys.jakub.companymanagement.domain.absences.leaverequests;
+
+
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.AcceptantId;
+
+public class Acceptant {
+    private final AcceptantId accptantId;
+    public Acceptant(AcceptantId accptantId) {
+        this.accptantId = accptantId;
+    }
+
+    public void accept(LeaveRequestAggregate leaveRequest) {
+        leaveRequest.markAsAccepted();
+    }
+
 }
