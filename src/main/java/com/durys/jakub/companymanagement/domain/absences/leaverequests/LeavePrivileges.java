cb6ec06010e2bde783ccbc1930e8c3e737726c04
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-29 20:58:44 +0100

    init of leaveRequestPrivileges model

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeavePrivileges.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeavePrivileges.java
@@ -1,4 +1,27 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
+import com.durys.jakub.companymanagement.commons.domain.Entity;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
+import com.durys.jakub.companymanagement.domain.employees.model.Employee;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
 public class LeavePrivileges {
+
+    private final LeaveRequestType leaveRequestType;
+    private final EmployeeId employeeId;
+
+    private LocalDateTime dateFrom;
+    private LocalDateTime dateTo;
+
+    private Integer daysEntitled;
+    private BigDecimal hoursEntitled;
+
+    private Integer daysUser;
+    private BigDecimal hoursUser;
+
 }
