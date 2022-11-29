00685e27075c536b9dc3960989bc24fce9eb1be7
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-29 20:19:05 +0100

    build fix

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/Acceptant.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/Acceptant.java
@@ -1,8 +1,12 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
 
+import com.durys.jakub.companymanagement.commons.domain.Entity;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.AcceptantId;
+import lombok.Getter;
 
+@Entity
+@Getter
 public class Acceptant {
     private final AcceptantId accptantId;
     public Acceptant(AcceptantId accptantId) {
