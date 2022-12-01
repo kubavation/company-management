0ba182a9a55b053e68db07095fa0f2a7fca4adb9
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-01 18:51:21 +0100

    fix in applicant/acceptant ids

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/vo/AuthorId.java
+++
@@ -1,9 +0,0 @@
-package com.durys.jakub.companymanagement.domain.absences.leaverequests.vo;
-
-import lombok.*;
-
-import java.io.Serializable;
-
-@Getter
-public record AuthorId(Long id) implements Serializable {
-}
--- src/main/java/com/durys/jakub/companymanagement/domain/employees/model/EmployeeId.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/employees/model/EmployeeId.java
@@ -7,10 +7,10 @@ import java.util.UUID;
 
 @Getter
 @RequiredArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @EqualsAndHashCode
 public class EmployeeId implements Serializable {
-    private final UUID id;
+    protected final UUID id;
 
     public static EmployeeId from(UUID id) {
         return new EmployeeId(id);
