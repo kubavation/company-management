b2145f29f72b631f5877c97aeedc26546fee05ee
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-03 09:37:30 +0100

    cleaning

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/ApplicantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.durys.jakub.companymanagement.domain.absences.leaverequests;
-
-import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.ApplicantId;
-
-public interface ApplicantRepository {
-
-    ApplicantId load(ApplicantId id);
-
-}
