5a141c9b2dfc8accbc2de02de61ef4ced9417b36
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-03 09:27:23 +0100

    endpoint for loading leaveRequest

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/leaverequests/in_adapters/LeaveRequestController.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/leaverequests/in_adapters/LeaveRequestController.java
@@ -1,10 +1,23 @@
 package com.durys.jakub.companymanagement.infrastructure.leaverequests.in_adapters;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.durys.jakub.companymanagement.application.leaverequests.LeaveRequestsApplicationService;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestId;
+import com.durys.jakub.companymanagement.infrastructure.leaverequests.dto.LeaveRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 public class LeaveRequestController {
-    
 
-    //todo
+    private final LeaveRequestsApplicationService leaveRequestsApplicationService;
+
+    @GetMapping("/employees/{employeeId}/leave-requestes/{leaveRequestId}")
+    LeaveRequestDTO findLeaveRequest(@PathVariable Long employeeId, @PathVariable UUID leaveRequestId) {
+        return leaveRequestsApplicationService.loadLeaveRequest(new LeaveRequestId(leaveRequestId));
+    }
+
 }
