f4461fc668a4cf10ead4088d81d7cb21cb587187
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-06 17:53:24 +0100

    tests

--- src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestPeriodTest.java
+++ src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestPeriodTest.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
 
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.InvalidLeaveRequestPeriodException;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,12 @@ class LeaveRequestPeriodTest {
         LocalDateTime currentDate = LocalDateTime.now();
         LeaveRequestPeriod leaveRequestPeriod = new LeaveRequestPeriod(currentDate, currentDate);
         assertEquals(1, leaveRequestPeriod.numberOfDays());
+    }
+
+    @Test
+    void createLeaveRequestPeriod_shouldThrowInvalidLeaveRequestPeriodException() {
+        LocalDateTime currentDate = LocalDateTime.now();
+        assertThrows(InvalidLeaveRequestPeriodException.class, () -> new LeaveRequestPeriod(currentDate, currentDate.minusSeconds(1)));
     }
 
 }
