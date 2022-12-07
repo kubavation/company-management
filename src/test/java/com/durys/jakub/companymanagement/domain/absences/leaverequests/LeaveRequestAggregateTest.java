694c61ffda244f4afd7d9e83e6239e9e8c5ec460
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-07 18:27:03 +0100

    tests for sending to acceptation

--- src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestAggregateTest.java
+++ src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestAggregateTest.java
@@ -1,6 +1,8 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.InvalidLeaveRequestPeriodException;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.InvalidStatusForOperationException;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.AcceptantId;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.ApplicantId;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestStatus;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
@@ -46,5 +48,37 @@ class LeaveRequestAggregateTest {
 
         assertThrows(InvalidStatusForOperationException.class, leaveRequestAggregate::markAsDeleted);
     }
+
+
+    @Test
+    void sendLeaveRequestToAcceptant_shouldSendToAcceptant() {
+
+        LeaveRequestAggregate leaveRequestAggregate = new LeaveRequestAggregate(
+                LeaveRequestType.AL, new Applicant(new ApplicantId(UUID.randomUUID())), new LeaveRequestPeriod(LocalDateTime.now(), LocalDateTime.now())
+        );
+
+        Acceptant acceptant = new Acceptant(new AcceptantId(UUID.randomUUID()));
+
+
+        leaveRequestAggregate.sendToAcceptant(acceptant);
+
+        assertEquals(LeaveRequestStatus.SEND_FOR_ACCEPTATION, leaveRequestAggregate.getStatus());
+        assertEquals(acceptant.getAccptantId(), leaveRequestAggregate.getAcceptant().getAccptantId());
+    }
+
+    @Test
+    void sendLeaveRequestToAcceptant_shouldThrowInvalidStatusForOperationException() {
+
+        LeaveRequestAggregate leaveRequestAggregate = new LeaveRequestAggregate(
+                LeaveRequestType.AL, new Applicant(new ApplicantId(UUID.randomUUID())), new LeaveRequestPeriod(LocalDateTime.now(), LocalDateTime.now())
+        );
+
+        leaveRequestAggregate.markAsDeleted();
+        Acceptant acceptant = new Acceptant(new AcceptantId(UUID.randomUUID()));
+
+
+        assertThrows(InvalidStatusForOperationException.class, () -> leaveRequestAggregate.sendToAcceptant(acceptant));
+    }
+
 
 }
