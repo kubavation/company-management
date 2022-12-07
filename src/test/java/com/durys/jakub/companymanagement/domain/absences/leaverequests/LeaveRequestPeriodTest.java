29385ecfa7b37e435d7b16c135ca0cfe133ab1c3
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-07 17:57:46 +0100

    more tests in period

--- src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestPeriodTest.java
+++ src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestPeriodTest.java
@@ -4,7 +4,10 @@ package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.InvalidLeaveRequestPeriodException;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAdjuster;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +17,6 @@ class LeaveRequestPeriodTest {
     @Test
     void createLeaveRequestPeriod_shouldCreate() {
         LocalDateTime currentDate = LocalDateTime.now();
-        LeaveRequestPeriod leaveRequestPeriod = new LeaveRequestPeriod(currentDate, currentDate);
         assertDoesNotThrow(() -> new LeaveRequestPeriod(currentDate, currentDate));
     }
 
@@ -22,6 +24,14 @@ class LeaveRequestPeriodTest {
     void createLeaveRequestPeriod_shouldThrowInvalidLeaveRequestPeriodException() {
         LocalDateTime currentDate = LocalDateTime.now();
         assertThrows(InvalidLeaveRequestPeriodException.class, () -> new LeaveRequestPeriod(currentDate, currentDate.minusSeconds(1)));
+    }
+
+    @Test
+    void getLeaveRequestPeriodNumberOfDays_shouldReturn1Day() {
+        LocalDateTime date1 = LocalDate.now().atTime(LocalTime.of(8,0));
+        LocalDateTime date2 = LocalDate.now().atTime(LocalTime.of(16,0));
+        LeaveRequestPeriod leaveRequestPeriod = new LeaveRequestPeriod(date1, date2);
+        assertEquals(1, leaveRequestPeriod.numberOfDays());
     }
 
 }
