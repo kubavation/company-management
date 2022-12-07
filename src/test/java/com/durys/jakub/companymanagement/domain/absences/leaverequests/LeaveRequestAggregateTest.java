45aa0fb2bcf9d5d3ab7f973c2a7d443b5a239a4f
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-07 18:22:05 +0100

    delete tests

--- src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestAggregateTest.java
+++ src/test/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestAggregateTest.java
@@ -1,5 +1,6 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.InvalidStatusForOperationException;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.ApplicantId;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestStatus;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
@@ -20,6 +21,30 @@ class LeaveRequestAggregateTest {
         );
 
         assertEquals(LeaveRequestStatus.SUBMITTED, leaveRequestAggregate.getStatus());
+    }
+
+
+    @Test
+    void deleteLeaveRequest_shouldMarkLeaveRequestAsDeleted() {
+        LeaveRequestAggregate leaveRequestAggregate = new LeaveRequestAggregate(
+                LeaveRequestType.AL, new Applicant(new ApplicantId(UUID.randomUUID())), new LeaveRequestPeriod(LocalDateTime.now(), LocalDateTime.now())
+        );
+
+        leaveRequestAggregate.markAsDeleted();
+
+        assertEquals(LeaveRequestStatus.DELETED, leaveRequestAggregate.getStatus());
+    }
+
+    @Test
+    void deleteLeaveRequest_shouldThrowInvalidStatusForOperationException() {
+        LeaveRequestAggregate leaveRequestAggregate = new LeaveRequestAggregate(
+                LeaveRequestType.AL, new Applicant(new ApplicantId(UUID.randomUUID())), new LeaveRequestPeriod(LocalDateTime.now(), LocalDateTime.now())
+        );
+        leaveRequestAggregate.markAsRejected();
+
+
+
+        assertThrows(InvalidStatusForOperationException.class, leaveRequestAggregate::markAsDeleted);
     }
 
 }
