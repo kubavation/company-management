54a31973121a1cae4393dfa2a8618907f94c6de9
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-11 20:39:21 +0100

    leave priviliges test

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeavePrivileges.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeavePrivileges.java
@@ -1,12 +1,14 @@
 package com.durys.jakub.companymanagement.domain.absences.leaveprivileges;
 
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.LeaveRequestAggregate;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.RequestedDaysExceedLeavePrivilegesException;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
-@AllArgsConstructor
+
 @Getter
 public class LeavePrivileges {
 
@@ -15,5 +17,20 @@ public class LeavePrivileges {
 
     private final LeavePrivilegesPeriod period;
     private final GrantedPrivileges grantedPrivileges;
+
+    public LeavePrivileges(LeaveRequestType leaveRequestType, EmployeeId employeeId, LeavePrivilegesPeriod period, GrantedPrivileges grantedPrivileges) {
+        this.leaveRequestType = leaveRequestType;
+        this.employeeId = employeeId;
+        this.period = period;
+        this.grantedPrivileges = grantedPrivileges;
+    }
+
+
+    public void checkCompatibility(LeaveRequestAggregate leaveRequestAggregate) {
+        Long numberOfDays = leaveRequestAggregate.getPeriod().numberOfDays();
+        if (numberOfDays > grantedPrivileges.getDaysEntitled()) {
+            throw new RequestedDaysExceedLeavePrivilegesException();
+        }
+    }
 
 }
---
+++ src/test/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeavePrivilegesTest.java
@@ -0,0 +1,39 @@
+package com.durys.jakub.companymanagement.domain.absences.leaveprivileges;
+
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.Applicant;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.LeaveRequestAggregate;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.LeaveRequestPeriod;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.RequestedDaysExceedLeavePrivilegesException;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.ApplicantId;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeavePrivilegesTest {
+
+    @Test
+    void checkCompatibility_shouldThrowRequestedDaysExceedLeavePrivilegesException() {
+
+        UUID employeeId = UUID.randomUUID();
+
+        LocalDate from = LocalDate.of(2022, 1 ,1);
+        LocalDate to = LocalDate.of(2022,12, 31);
+
+        LeavePrivileges leavePrivileges = new LeavePrivileges(LeaveRequestType.AL, new EmployeeId(employeeId),
+                new LeavePrivilegesPeriod(from, to),
+                new GrantedPrivileges(1, 10)
+        );
+
+        LeaveRequestAggregate leaveRequestAggregate = new LeaveRequestAggregate(LeaveRequestType.AL,
+                new Applicant(new ApplicantId(employeeId)),
+                new LeaveRequestPeriod(from.atStartOfDay().plusDays(10), from.atStartOfDay().plusDays(20)));
+        
+        assertThrows(RequestedDaysExceedLeavePrivilegesException.class, () -> leavePrivileges.checkCompatibility(leaveRequestAggregate));
+    }
+}
