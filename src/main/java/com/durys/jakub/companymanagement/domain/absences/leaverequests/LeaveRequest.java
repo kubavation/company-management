a6296433b49c398b51e0138532bd70611ca5afdc
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-12 17:06:45 +0100

    init of daily/hourly leave request

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequest.java
@@ -1,4 +1,19 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestId;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
+
 public abstract class LeaveRequest {
+
+    private final LeaveRequestId requestId;
+    private final LeaveRequestType requestType;
+    private final Applicant applicant;
+
+    private Acceptant acceptant;
+
+    public LeaveRequest(LeaveRequestId requestId, LeaveRequestType requestType, Applicant applicant) {
+        this.requestId = requestId;
+        this.requestType = requestType;
+        this.applicant = applicant;
+    }
 }
