ab1a6992f4de786473d6aaa41f6f441ae34e3f54
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-12 17:08:38 +0100

    added status field to leaveRequest entity

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequest.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestId;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestStatus;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
 
 public abstract class LeaveRequest {
@@ -11,9 +12,12 @@ public abstract class LeaveRequest {
 
     private Acceptant acceptant;
 
+    private LeaveRequestStatus status;
+
     public LeaveRequest(LeaveRequestId requestId, LeaveRequestType requestType, Applicant applicant) {
         this.requestId = requestId;
         this.requestType = requestType;
         this.applicant = applicant;
+        this.status = LeaveRequestStatus.SUBMITTED;
     }
 }
