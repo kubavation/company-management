0f14dfffa0fc68afcabb106bd17f29210bbd7b1d
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-14 20:27:37 +0100

    change in period

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestHourlyPeriod.java
@@ -0,0 +1,21 @@
+package com.durys.jakub.companymanagement.domain.absences.leaverequests;
+
+import com.durys.jakub.companymanagement.commons.domain.ValueObject;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@ValueObject
+public class LeaveRequestHourlyPeriod extends LeaveRequestPeriod {
+
+
+    protected LeaveRequestHourlyPeriod(LocalDateTime dateFrom, LocalDateTime dateTo) {
+        super(dateFrom, dateTo);
+    }
+
+    @Override
+    public BigDecimal quantity() {
+        return BigDecimal.valueOf(ChronoUnit.DAYS.between(from, to) + 1);
+    }
+}
