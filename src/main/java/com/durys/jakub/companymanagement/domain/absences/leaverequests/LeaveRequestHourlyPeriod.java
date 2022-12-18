740ccb13e07e8c8b9f60b12d7edf2997b5548d03
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-18 11:05:57 +0100

    validation in hourlyPeriod

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestHourlyPeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/LeaveRequestHourlyPeriod.java
@@ -4,15 +4,24 @@ import com.durys.jakub.companymanagement.commons.domain.ValueObject;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.InvalidLeaveRequestPeriodException;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
 @ValueObject
 public class LeaveRequestHourlyPeriod extends LeaveRequestPeriod {
 
 
-    public LeaveRequestHourlyPeriod(LocalDateTime dateFrom, LocalDateTime dateTo) {
-        super(dateFrom, dateTo);
+    public static LeaveRequestHourlyPeriod from(LocalDateTime from, LocalDateTime to) {
+        Objects.requireNonNull(from, "Date from value not provided");
+        Objects.requireNonNull(to, "Date from value not provided");
+
+        return new LeaveRequestHourlyPeriod(from, to);
+    }
+
+    private LeaveRequestHourlyPeriod(LocalDateTime from, LocalDateTime to) {
+        super(from, to);
     }
 
     @Override
