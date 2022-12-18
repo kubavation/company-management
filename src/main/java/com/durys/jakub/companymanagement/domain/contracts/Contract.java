0c08a73318fd1b65922601316cb5c77b025fabba
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-18 20:16:35 +0100

    employeeId field in contract

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/Contract.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/Contract.java
@@ -1,5 +1,14 @@
 package com.durys.jakub.companymanagement.domain.contracts;
 
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Contract {
 
+    private final ContractId contractId;
+
+    private final EmployeeId employeeId;
+
+    
 }
