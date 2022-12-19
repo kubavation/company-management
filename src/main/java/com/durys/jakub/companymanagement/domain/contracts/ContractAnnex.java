c950ca63dfeef0a72a5268aa34d9c0e7e7565b06
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-19 18:39:22 +0100

    init of contract annex

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractAnnex.java
@@ -0,0 +1,11 @@
+package com.durys.jakub.companymanagement.domain.contracts;
+
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ContractAnnex {
+
+    private final ContractId contractId;
+
+}
