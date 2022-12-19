77630ac28a4fd1e4712e26ffe391725031d42dfa
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-19 19:05:51 +0100

    Salary vo

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/Salary.java
@@ -0,0 +1,15 @@
+package com.durys.jakub.companymanagement.domain.contracts.vo;
+
+import com.durys.jakub.companymanagement.commons.vo.Currency;
+import com.durys.jakub.companymanagement.commons.vo.Money;
+
+public class Salary {
+
+    private final Money amount;
+    private final Currency currency;
+
+    public Salary(Money amount, Currency currency) {
+       this.amount = amount;
+       this.currency = currency;
+    }
+}
