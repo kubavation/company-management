b746a1c88bc63e5df0e49c3c857b9a246dc840c0
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-19 19:35:56 +0100

    working time vo

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/WorkingTime.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/WorkingTime.java
@@ -4,8 +4,10 @@ package com.durys.jakub.companymanagement.domain.contracts.vo;
 public class WorkingTime {
 
     private final DailyHourNumber dailyHourNumber;
+    private final BillingPeriod billingPeriod;
 
-    public WorkingTime(DailyHourNumber dailyHourNumber) {
+    public WorkingTime(DailyHourNumber dailyHourNumber, BillingPeriod billingPeriod) {
         this.dailyHourNumber = dailyHourNumber;
+        this.billingPeriod = billingPeriod;
     }
 }
