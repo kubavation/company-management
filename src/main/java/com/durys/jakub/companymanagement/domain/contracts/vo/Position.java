eb52123dc5c5c64c16fd8518aa29b041f86ec31c
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-19 19:01:00 +0100

    position vo

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/Position.java
@@ -0,0 +1,12 @@
+package com.durys.jakub.companymanagement.domain.contracts.vo;
+
+import java.util.Objects;
+
+public class Position {
+    private final String name;
+
+    public Position(String name) {
+        Objects.requireNonNull(name);
+        this.name = name;
+    }
+}
