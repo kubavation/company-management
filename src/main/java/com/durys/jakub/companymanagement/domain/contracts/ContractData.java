2d0a414eb7593ece645459dc6c32a9dfa65fdea7
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-19 18:57:01 +0100

    init of contract data

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractData.java
@@ -0,0 +1,12 @@
+package com.durys.jakub.companymanagement.domain.contracts;
+
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class ContractData {
+
+}
