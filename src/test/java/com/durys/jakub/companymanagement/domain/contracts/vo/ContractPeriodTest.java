28d4cccd654460fe307f8868afa04fffa8e719a8
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-20 18:50:59 +0100

    contract period - null tests

--- src/test/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriodTest.java
+++ src/test/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriodTest.java
@@ -17,7 +17,7 @@ class ContractPeriodTest {
     }
 
     @Test
-    void createContractPeriod_shouldThrowException_whenPeriodTypeIsEmpty() {
+    void createContractPeriod_shouldThrowException_whenPeriodTypeIsNotDefined() {
 
         LocalDate from = LocalDate.of(2000, Month.JANUARY, 1);
         LocalDate to = LocalDate.of(2001, Month.JANUARY, 1);
@@ -26,4 +26,25 @@ class ContractPeriodTest {
 
         assertEquals("invalid period type param", exception.getMessage());
     }
+
+    @Test
+    void createContractPeriod_shouldThrowException_whenInvalidDatesPeriod() {
+
+        LocalDate from = LocalDate.of(2001, Month.JANUARY, 1);
+        LocalDate to = LocalDate.of(2000, Month.JANUARY, 1);
+
+        assertThrows(InvalidContractPeriodException.class, () -> new ContractPeriod(ContractPeriodType.FIXED_TERM, from, to));
+    }
+
+    @Test
+    void createContractPeriod_shouldThrowException_whenDateFromIsNotDefined() {
+
+        LocalDate from = null;
+        LocalDate to = LocalDate.of(2001, Month.JANUARY, 1);
+
+        Exception exception = assertThrows(RuntimeException.class, () -> new ContractPeriod(null, from, to));
+
+        assertEquals("invalid date from param", exception.getMessage());
+    }
+    
 }
