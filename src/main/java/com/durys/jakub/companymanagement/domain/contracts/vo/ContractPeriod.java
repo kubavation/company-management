72ca45f569e9009b9dd0587bdc8dc2059104a2f5
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-20 19:25:38 +0100

    contract period validation #2

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriod.java
@@ -1,6 +1,9 @@
 package com.durys.jakub.companymanagement.domain.contracts.vo;
 
 
+import com.durys.jakub.companymanagement.domain.contracts.exception.ContractPeriodEndDateIncorrectlyDefined;
+import com.durys.jakub.companymanagement.domain.contracts.exception.ContractPeriodEndDateNotDefinedException;
+
 import java.time.LocalDate;
 import java.util.Objects;
 
@@ -15,8 +18,22 @@ class ContractPeriod {
         Objects.requireNonNull(type, "invalid period type param");
         Objects.requireNonNull(from, "invalid date from param");
 
+        validateContractPeriodByType(type, to);
+
         this.type = type;
         this.from = from;
         this.to = to;
+    }
+
+    private void validateContractPeriodByType(ContractPeriodType type, LocalDate to) {
+
+        if (ContractPeriodType.FIXED_TERM.equals(type) && Objects.isNull(to)) {
+            throw new ContractPeriodEndDateNotDefinedException();
+        }
+
+        if (ContractPeriodType.PERMANENT.equals(type) && Objects.nonNull(to)) {
+            throw new ContractPeriodEndDateIncorrectlyDefined();
+        }
+
     }
 }
