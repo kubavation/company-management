3bb119e3f68cd5e6407e4ddeb30c6b2b2c9cae78
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-20 18:22:40 +0100

    contractPeriodtype as field in contractPeriod

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriod.java
@@ -4,10 +4,13 @@ package com.durys.jakub.companymanagement.domain.contracts.vo;
 import java.time.LocalDate;
 
 class ContractPeriod {
+
+    private final ContractPeriodType type;
     private final LocalDate from;
     private final LocalDate to;
 
-    ContractPeriod(LocalDate from, LocalDate to) {
+    ContractPeriod(ContractPeriodType type, LocalDate from, LocalDate to) {
+        this.type = type;
         this.from = from;
         this.to = to;
     }
