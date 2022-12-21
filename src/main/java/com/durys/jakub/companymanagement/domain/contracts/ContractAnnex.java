678b9f02cb314bef48d02d9b2071cc2ef289e934
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-21 17:27:53 +0100

    additional contractAnnex constructor

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractAnnex.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractAnnex.java
@@ -2,6 +2,8 @@ package com.durys.jakub.companymanagement.domain.contracts;
 
 import com.durys.jakub.companymanagement.domain.contracts.vo.ContractData;
 
+import java.util.UUID;
+
 public class ContractAnnex {
 
     private final AnnexId annexId;
@@ -10,6 +12,12 @@ public class ContractAnnex {
 
     public ContractAnnex(AnnexId annexId, ContractData contractData, ContractId contractId) {
         this.annexId = annexId;
+        this.contractData = contractData;
+        this.contractId = contractId;
+    }
+
+    public ContractAnnex(ContractData contractData, ContractId contractId) {
+        this.annexId = new AnnexId(UUID.randomUUID());
         this.contractData = contractData;
         this.contractId = contractId;
     }
