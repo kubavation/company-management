3fb95a5d4d4acbe7ccb51c12dc8a115d397a950f
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-22 19:10:54 +0100

    init of employeesApplicationService

---
+++ src/main/java/com/durys/jakub/companymanagement/application/employees/EmployeesApplicationService.java
@@ -0,0 +1,9 @@
+package com.durys.jakub.companymanagement.application.employees;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeesApplicationService {
+}
