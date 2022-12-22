cc4c2bc6319ba3dad48e0244878251acfc8c1b32
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-22 19:00:26 +0100

    init of employee controler in input adapters

---
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.durys.jakub.companymanagement.infrastructure.employees.input;
+
+import com.durys.jakub.companymanagement.application.leaverequests.LeaveRequestsApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+    
+
+}
