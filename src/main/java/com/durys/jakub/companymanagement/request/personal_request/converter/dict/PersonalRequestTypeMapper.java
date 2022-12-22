4db98864e07e01dfc6d8a415ccba93ec57a19991
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-22 18:50:09 +0100

    build fix

--- src/main/java/com/durys/jakub/companymanagement/request/personal_request/converter/dict/PersonalRequestTypeMapper.java
+++ src/main/java/com/durys/jakub/companymanagement/request/personal_request/converter/dict/PersonalRequestTypeMapper.java
@@ -1,20 +1,20 @@
-package com.durys.jakub.companymanagement.request.personal_request.converter.dict;
-
-import com.durys.jakub.companymanagement.request.personal_request.model.dto.dict.PersonalRequestTypeDTO;
-import com.durys.jakub.companymanagement.request.personal_request.model.entity.dict.PersonalRequestType;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
-
-import java.util.List;
-
-@Mapper(componentModel = "spring", uses = {PersonalRequestFieldTypeMapper.class})
-public interface PersonalRequestTypeMapper {
-
-    PersonalRequestTypeMapper INSTANCE = Mappers.getMapper( PersonalRequestTypeMapper.class );
-
-    @Mapping(source = "entity.requestFieldTypes", target = "requestFieldTypes")
-    PersonalRequestTypeDTO toDTO(PersonalRequestType entity);
-
-    List<PersonalRequestTypeDTO> toDTO(List<PersonalRequestType> entity);
-}
+//package com.durys.jakub.companymanagement.request.personal_request.converter.dict;
+//
+//import com.durys.jakub.companymanagement.request.personal_request.model.dto.dict.PersonalRequestTypeDTO;
+//import com.durys.jakub.companymanagement.request.personal_request.model.entity.dict.PersonalRequestType;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.factory.Mappers;
+//
+//import java.util.List;
+//
+//@Mapper(componentModel = "spring", uses = {PersonalRequestFieldTypeMapper.class})
+//public interface PersonalRequestTypeMapper {
+//
+//    PersonalRequestTypeMapper INSTANCE = Mappers.getMapper( PersonalRequestTypeMapper.class );
+//
+//    @Mapping(source = "entity.requestFieldTypes", target = "requestFieldTypes")
+//    PersonalRequestTypeDTO toDTO(PersonalRequestType entity);
+//
+//    List<PersonalRequestTypeDTO> toDTO(List<PersonalRequestType> entity);
+//}
--- src/main/java/com/durys/jakub/companymanagement/request/personal_request/converter/general/PersonalRequestFieldMapper.java
+++ src/main/java/com/durys/jakub/companymanagement/request/personal_request/converter/general/PersonalRequestFieldMapper.java
@@ -1,31 +1,31 @@
-package com.durys.jakub.companymanagement.request.personal_request.converter.general;
-
-import com.durys.jakub.companymanagement.request.personal_request.model.dto.general.PersonalRequestFieldDTO;
-import com.durys.jakub.companymanagement.request.personal_request.model.entity.general.PersonalRequestField;
-import com.durys.jakub.companymanagement.request.personal_request.service.utils.FieldTypeListUtil;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-
-@Mapper(componentModel = "spring")
-public abstract class PersonalRequestFieldMapper {
-
-    @Autowired
-    protected FieldTypeListUtil fieldTypeListUtil;
-
-
-    @Mappings({
-        @Mapping(source = "requestFieldType.id", target = "fieldTypeId"),
-        @Mapping(source = "requestFieldType.name", target = "name"),
-        @Mapping(source = "requestFieldType.type", target = "type"),
-        @Mapping(source = "requestFieldType.label", target = "label"),
-        @Mapping(target = "requestFieldType.list", expression = "java(fieldTypeListUtil.generateListValues(entity.getListQuery()))"),
-    })
-    public abstract PersonalRequestFieldDTO toDTO(PersonalRequestField entity);
-
-    public abstract List<PersonalRequestFieldDTO> toDTO(List<PersonalRequestField> entities);
-
-}
+//package com.durys.jakub.companymanagement.request.personal_request.converter.general;
+//
+//import com.durys.jakub.companymanagement.request.personal_request.model.dto.general.PersonalRequestFieldDTO;
+//import com.durys.jakub.companymanagement.request.personal_request.model.entity.general.PersonalRequestField;
+//import com.durys.jakub.companymanagement.request.personal_request.service.utils.FieldTypeListUtil;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.Mappings;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import java.util.List;
+//
+//@Mapper(componentModel = "spring")
+//public abstract class PersonalRequestFieldMapper {
+//
+//    @Autowired
+//    protected FieldTypeListUtil fieldTypeListUtil;
+//
+//
+//    @Mappings({
+//        @Mapping(source = "requestFieldType.id", target = "fieldTypeId"),
+//        @Mapping(source = "requestFieldType.name", target = "name"),
+//        @Mapping(source = "requestFieldType.type", target = "type"),
+//        @Mapping(source = "requestFieldType.label", target = "label"),
+//        @Mapping(target = "requestFieldType.list", expression = "java(fieldTypeListUtil.generateListValues(entity.getListQuery()))"),
+//    })
+//    public abstract PersonalRequestFieldDTO toDTO(PersonalRequestField entity);
+//
+//    public abstract List<PersonalRequestFieldDTO> toDTO(List<PersonalRequestField> entities);
+//
+//}
