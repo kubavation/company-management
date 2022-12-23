9a1d6c431370dd88f4cd8ad84ff5a1f435e77558
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-23 22:39:09 +0100

    employeApplicationService tests #1

--- src/test/java/com/durys/jakub/companymanagement/application/employees/EmployeesApplicationServiceTest.java
+++ src/test/java/com/durys/jakub/companymanagement/application/employees/EmployeesApplicationServiceTest.java
@@ -1,17 +1,42 @@
 package com.durys.jakub.companymanagement.application.employees;
 
+import com.durys.jakub.companymanagement.domain.employees.model.Employee;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeRepository;
 import com.durys.jakub.companymanagement.infrastructure.employees.EmployeesConfiguration;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 class EmployeesApplicationServiceTest {
 
     private final EmployeesConfiguration employeesConfiguration = new EmployeesConfiguration();
+
     private final EmployeeRepository employeeRepository = employeesConfiguration.hashMapEmployeeRepository();
+
     private final EmployeesApplicationService employeesApplicationService = new EmployeesConfiguration().employeesApplicationService(employeeRepository);
 
 
-    
+    @Test
+    void employPerson_shouldSuccessfullyCreateEmployee() {
+
+        final UUID nextId = UUID.randomUUID();
+
+        when(employeeRepository.nextId()).thenReturn(nextId);
+
+        employeesApplicationService.employ("Dave", "James", "MAN");
+
+        Employee employee = employeeRepository.load(new EmployeeId(nextId));
+
+        assertEquals("James Dave", employee.name());
+    }
+
 
 }
