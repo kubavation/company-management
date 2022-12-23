f20f576b4006cb98a04237275a253530f6f2a563
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-23 20:48:58 +0100

    create employee endpoint

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/EmployeeController.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/EmployeeController.java
@@ -1,13 +1,21 @@
 package com.durys.jakub.companymanagement.infrastructure.employees.input;
 
+import com.durys.jakub.companymanagement.application.employees.EmployeesApplicationService;
+import com.durys.jakub.companymanagement.infrastructure.employees.input.model.EmployeeDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
+    private final EmployeesApplicationService employeesApplicationService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody EmployeeDTO employeeDTO) {
+        employeesApplicationService.employ(employeeDTO.getFirstName(), employeeDTO.getLastName(), employeeDTO.getGender());
+    }
 }
