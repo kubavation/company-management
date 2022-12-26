db9e3e651106223a37630d4968ff605504b1b06b
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-26 20:23:38 +0100

    init of cqrs config + springCommandHandler

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/cqrs/commands/SpringCommandHandlerProvider.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/cqrs/commands/SpringCommandHandlerProvider.java
@@ -1,4 +1,19 @@
 package com.durys.jakub.companymanagement.infrastructure.cqrs.commands;
 
-public class SpringCommandHandlerProvider {
+import com.durys.jakub.companymanagement.cqrs.commands.Command;
+import com.durys.jakub.companymanagement.cqrs.commands.CommandHandler;
+import com.durys.jakub.companymanagement.cqrs.commands.CommandHandlerProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpringCommandHandlerProvider implements CommandHandlerProvider {
+
+
+    private final Map<String, Class<?>> handlers = new HashMap<>();
+
+    @Override
+    public <T extends Command> CommandHandler<T> getCommandHandler(T command) {
+        return null;
+    }
 }
