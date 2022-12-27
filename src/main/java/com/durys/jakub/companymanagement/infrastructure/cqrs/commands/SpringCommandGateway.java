2d8096bbb7dc7ef8f5c78523e0d60c61caf17dae
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-27 18:33:31 +0100

    spring command gateway impl

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/cqrs/commands/SpringCommandGateway.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/cqrs/commands/SpringCommandGateway.java
@@ -2,11 +2,19 @@ package com.durys.jakub.companymanagement.infrastructure.cqrs.commands;
 
 import com.durys.jakub.companymanagement.cqrs.commands.Command;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandGateway;
+import com.durys.jakub.companymanagement.cqrs.commands.CommandHandlerProvider;
 
 public class SpringCommandGateway implements CommandGateway {
 
+    private final CommandHandlerProvider commandHandlerProvider;
+
+
+    public SpringCommandGateway(CommandHandlerProvider commandHandlerProvider) {
+        this.commandHandlerProvider = commandHandlerProvider;
+    }
+
     @Override
     public <T extends Command> void dispatch(T command) {
-        
+        commandHandlerProvider.getCommandHandlerFor(command).handle(command);
     }
 }
