19c5d6b4ee7a3535c1750fc8c695deb6c7ceae89
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-27 16:34:14 +0100

    register springCommandHandlerProvider

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/cqrs/commands/SpringCommandHandlerProvider.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/cqrs/commands/SpringCommandHandlerProvider.java
@@ -3,17 +3,31 @@ package com.durys.jakub.companymanagement.infrastructure.cqrs.commands;
 import com.durys.jakub.companymanagement.cqrs.commands.Command;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandler;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandlerProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class SpringCommandHandlerProvider implements CommandHandlerProvider {
 
+    private final ConfigurableListableBeanFactory configurableListableBeanFactory;
 
     private final Map<String, Class<?>> handlers = new HashMap<>();
+
+    public SpringCommandHandlerProvider(ConfigurableListableBeanFactory configurableListableBeanFactory) {
+        this.configurableListableBeanFactory = configurableListableBeanFactory;
+    }
 
     @Override
     public <T extends Command> CommandHandler<T> getCommandHandler(T command) {
         return null;
+    }
+
+
+    private void prepareHandlers() {
+        //todo
     }
 }
