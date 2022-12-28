734eaceea8c17ad4f79ad9f326eb1bf31a94cfef
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-28 19:48:27 +0100

    change in grantLeaveRequestPrivilegesCommandHandler

--- src/main/java/com/durys/jakub/companymanagement/application/absences/leaveprivileges/commands/handlers/GrantLeaveRequestPrivilegesCommandHandler.java
+++ src/main/java/com/durys/jakub/companymanagement/application/absences/leaveprivileges/commands/handlers/GrantLeaveRequestPrivilegesCommandHandler.java
@@ -3,32 +3,27 @@ package com.durys.jakub.companymanagement.application.absences.leaveprivileges.c
 import com.durys.jakub.companymanagement.application.absences.leaveprivileges.commands.GrantLeaveRequestPrivilegesCommand;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandler;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandling;
-import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.GrantedPrivileges;
-import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeavePrivilege;
-import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeavePrivilegesPeriod;
-import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeavePrivilegesRepository;
+import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.*;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
 import com.durys.jakub.companymanagement.domain.employees.model.Employee;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
-import com.durys.jakub.companymanagement.domain.employees.model.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 
 @CommandHandling
 @RequiredArgsConstructor
 public class GrantLeaveRequestPrivilegesCommandHandler implements CommandHandler<GrantLeaveRequestPrivilegesCommand> {
 
-    private final EmployeeRepository employeeRepository;
-    private final LeavePrivilegesRepository leavePrivilegesRepository;
+    private final LeaveEntitlementEmployeeRepository leaveEntitlementEmployeeRepository;
 
     @Override
     public void handle(GrantLeaveRequestPrivilegesCommand command) {
 
-        Employee employee = employeeRepository.load(EmployeeId.from(command.getEmployeeId()));
+        LeaveEntitlementEmployee employee = leaveEntitlementEmployeeRepository.load(EmployeeId.from(command.getEmployeeId()));
 
-        LeavePrivilege leavePrivilege = new LeavePrivilege(LeaveRequestType.valueOf(command.getLeaveType())
+        employee.grantWith(LeaveRequestType.valueOf(command.getLeaveType()),
                 new LeavePrivilegesPeriod(command.getFrom(), command.getTo()),
                 new GrantedPrivileges(command.getDaysEntitled(), command.getHoursEntitled()));
 
-        leavePrivilegesRepository.save(leavePrivilege);
+        leaveEntitlementEmployeeRepository.save(employee);
     }
 }
