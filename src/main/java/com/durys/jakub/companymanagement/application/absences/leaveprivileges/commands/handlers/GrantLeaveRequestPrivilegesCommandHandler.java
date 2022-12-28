dbcf5684fdfd5e8e4085d1cb3269070fd6ad2c70
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-28 17:53:30 +0100

    some fixes

--- src/main/java/com/durys/jakub/companymanagement/application/absences/leaveprivileges/commands/handlers/GrantLeaveRequestPrivilegesCommandHandler.java
+++ src/main/java/com/durys/jakub/companymanagement/application/absences/leaveprivileges/commands/handlers/GrantLeaveRequestPrivilegesCommandHandler.java
@@ -3,13 +3,13 @@ package com.durys.jakub.companymanagement.application.absences.leaveprivileges.c
 import com.durys.jakub.companymanagement.application.absences.leaveprivileges.commands.GrantLeaveRequestPrivilegesCommand;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandler;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandling;
+import lombok.RequiredArgsConstructor;
 
 @CommandHandling
 public class GrantLeaveRequestPrivilegesCommandHandler implements CommandHandler<GrantLeaveRequestPrivilegesCommand> {
 
-
     @Override
-    public void handle(GrantLeaveRequestPrivilegesCommand grantLeaveRequestPrivilegesCommand) {
-        //TODO
+    public void handle(GrantLeaveRequestPrivilegesCommand command) {
+
     }
 }
