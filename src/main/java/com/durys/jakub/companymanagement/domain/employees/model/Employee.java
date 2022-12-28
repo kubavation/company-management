5a84e5f8b0061ba67cc660c022b1b11006cea670
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-28 18:54:20 +0100

    rename annotation to @AggregateRoot

--- src/main/java/com/durys/jakub/companymanagement/domain/employees/model/Employee.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/employees/model/Employee.java
@@ -1,11 +1,9 @@
 package com.durys.jakub.companymanagement.domain.employees.model;
 
-import com.durys.jakub.companymanagement.commons.domain.Aggregate;
+import com.durys.jakub.companymanagement.commons.domain.AggregateRoot;
 import com.durys.jakub.companymanagement.domain.employees.model.vo.EmployeePersonalData;
 
-import java.util.UUID;
-
-@Aggregate
+@AggregateRoot
 public class Employee implements Employable {
     private final EmployeeId employeeId;
     private final EmployeePersonalData employeePersonalData;
