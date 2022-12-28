2d120921ee26bbf00834a4ce7c01047eab4e8d42
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-28 19:29:06 +0100

    employable leaveEntitlementEmployee

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeaveEntitlementEmployee.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeaveEntitlementEmployee.java
@@ -1,13 +1,14 @@
 package com.durys.jakub.companymanagement.domain.absences.leaveprivileges;
 
 import com.durys.jakub.companymanagement.commons.domain.AggregateRoot;
+import com.durys.jakub.companymanagement.domain.employees.model.Employable;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 
 import java.util.Collections;
 import java.util.List;
 
 @AggregateRoot
-public class LeaveEntitlementEmployee {
+public class LeaveEntitlementEmployee implements Employable {
 
     private final EmployeeId employeeId;
     private List<LeavePrivilege> leavePrivileges;
@@ -21,5 +22,14 @@ public class LeaveEntitlementEmployee {
     public LeaveEntitlementEmployee(EmployeeId employeeId) {
         this.employeeId = employeeId;
         this.leavePrivileges = Collections.emptyList();
+    }
+
+    void grant(LeavePrivilege leavePrivilege) {
+
+    }
+
+    @Override
+    public EmployeeId getId() {
+        return employeeId;
     }
 }
