17ad8450e4ef6c05bf061ae0f8778f199e64ed5c
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-29 18:48:27 +0100

    inMemory leaveEntitylementEmployee repository

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaveprivileges/ApplicantLeavePrivilegesService.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaveprivileges/ApplicantLeavePrivilegesService.java
@@ -1,19 +1,19 @@
-package com.durys.jakub.companymanagement.infrastructure.absences.leaveprivileges;
-
-import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeavePrivilege;
-import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.ApplicantId;
-import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
-import lombok.AllArgsConstructor;
-
-import java.time.LocalDate;
-
-@AllArgsConstructor
-public class ApplicantLeavePrivilegesService implements LeavePrivilegesService {
-
-    private final LeavePrivilegesRepository leavePrivilegesRepository;
-
-    @Override
-    public LeavePrivilege load(ApplicantId applicantId, LeaveRequestType requestType, LocalDate date) {
-        return leavePrivilegesRepository.load(applicantId, requestType, date);
-    }
-}
+//package com.durys.jakub.companymanagement.infrastructure.absences.leaveprivileges;
+//
+//import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeavePrivilege;
+//import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.ApplicantId;
+//import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
+//import lombok.AllArgsConstructor;
+//
+//import java.time.LocalDate;
+//
+//@AllArgsConstructor
+//public class ApplicantLeavePrivilegesService implements LeavePrivilegesService {
+//
+//    private final LeavePrivilegesRepository leavePrivilegesRepository;
+//
+//    @Override
+//    public LeavePrivilege load(ApplicantId applicantId, LeaveRequestType requestType, LocalDate date) {
+//        return leavePrivilegesRepository.load(applicantId, requestType, date);
+//    }
+//}
