41607f8c91b36afe6dd11a371df2e880e080221c
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-29 20:42:35 +0100

    SendLeaveRequestToAcceptantCommand

---
+++ src/main/java/com/durys/jakub/companymanagement/application/absences/leaverequests/commands/SendLeaveRequestToAcceptantCommand.java
@@ -0,0 +1,8 @@
+package com.durys.jakub.companymanagement.application.absences.leaverequests.commands;
+
+import java.util.UUID;
+
+public class SendLeaveRequestToAcceptantCommand {
+    private UUID leaveRequestId;
+    private UUID acceptantId;
+}
