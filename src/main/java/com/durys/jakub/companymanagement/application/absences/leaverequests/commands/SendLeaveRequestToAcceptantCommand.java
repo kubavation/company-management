94f52c6032cf4f62586f780f717ea08ea337a0af
kubavation <37585627+kubavation@users.noreply.github.com>
2022-12-29 20:55:55 +0100

    sendToAcceptant command handler

--- src/main/java/com/durys/jakub/companymanagement/application/absences/leaverequests/commands/SendLeaveRequestToAcceptantCommand.java
+++
@@ -1,8 +0,0 @@
-package com.durys.jakub.companymanagement.application.absences.leaverequests.commands;
-
-import java.util.UUID;
-
-public class SendLeaveRequestToAcceptantCommand {
-    private UUID leaveRequestId;
-    private UUID acceptantId;
-}
