13d58c31d7cd978e2bc483e0f653c762ae15b3d2
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-01 18:29:26 +0100

    checkCompatibilty method in leavePrivilege entity

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeavePrivilege.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeavePrivilege.java
@@ -2,8 +2,13 @@ package com.durys.jakub.companymanagement.domain.absences.leaveprivileges;
 
 import com.durys.jakub.companymanagement.commons.domain.ValueObject;
 import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.vo.LeaveType;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.DailyLeaveRequest;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.HourlyLeaveRequest;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.LeaveRequest;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.exception.RequestedDaysExceedLeavePrivilegesException;
 import lombok.Getter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 
@@ -25,24 +30,18 @@ class LeavePrivilege {
         return period.includes(date);
     }
 
-//
-//    public void checkCompatibility(LeaveRequest leaveRequest) {
-//
-////        if (!leaveType.equals(leaveRequest.getRequestType())) {
-////            throw new RuntimeException("Invalid requestType param");
-////        }
-//
-//        BigDecimal quantity = leaveRequest.getPeriod().getQuantity();
-//
-//        if (leaveRequest instanceof DailyLeaveRequest && quantity.compareTo(grantedPrivileges.getDaysEntitled()) > 0) {
-//            throw new RequestedDaysExceedLeavePrivilegesException();
-//        }
-//
-//        if (leaveRequest instanceof HourlyLeaveRequest && quantity.compareTo(grantedPrivileges.getHoursEntitled()) > 0) {
-//            throw new RequestedDaysExceedLeavePrivilegesException();
-//        }
-//
-//
-//    }
+    public void checkCompatibility(LeaveRequest leaveRequest) {
+
+        BigDecimal quantity = leaveRequest.getPeriod().getQuantity();
+
+        if (leaveRequest instanceof DailyLeaveRequest && quantity.compareTo(grantedPrivileges.getDaysEntitled()) > 0) {
+            throw new RequestedDaysExceedLeavePrivilegesException();
+        }
+
+        if (leaveRequest instanceof HourlyLeaveRequest && quantity.compareTo(grantedPrivileges.getHoursEntitled()) > 0) {
+            throw new RequestedDaysExceedLeavePrivilegesException();
+        }
+
+    }
 
 }
