fc1d0f87f53eacaaeef35e6157e6c2b436e15511
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-01 14:46:20 +0100

    cleaning in accepntant aggregate + annotation

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/Acceptant.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/Acceptant.java
@@ -1,19 +1,16 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
 
-import com.durys.jakub.companymanagement.commons.domain.DomainEntity;
+import com.durys.jakub.companymanagement.commons.domain.AggregateRoot;
 import com.durys.jakub.companymanagement.domain.employees.model.Employable;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@DomainEntity
-@Getter
+@AggregateRoot
+@RequiredArgsConstructor
 public class Acceptant implements Employable {
     private final AcceptantId accptantId;
 
-    public Acceptant(AcceptantId accptantId) {
-        this.accptantId = accptantId;
-    }
 
     public void accept(LeaveRequest leaveRequest) {
         leaveRequest.markAsAccepted();
