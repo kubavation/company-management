7cbd118717467af0b555162ad0afea8f9835065b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-02 18:51:37 +0100

    entitledAmountFrom implementation in HourlyLeaveRequest

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/HourlyLeaveRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/HourlyLeaveRequest.java
@@ -1,10 +1,22 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests;
 
+import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeaveEntitlements;
+import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.vo.LeaveType;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
+
+import java.math.BigDecimal;
 
 public class HourlyLeaveRequest extends LeaveRequest {
 
     public HourlyLeaveRequest(LeaveRequestId leaveRequestId, LeaveRequestType requestType, LeaveRequestHourlyPeriod period, ApplicantId applicantId) {
         super(leaveRequestId, requestType, period, applicantId);
+    }
+
+    @Override
+    BigDecimal entitledAmountFrom(LeaveEntitlements leaveEntitlements) {
+
+        LeaveType leaveType = LeaveType.valueOf(super.getRequestType().getName());
+
+        return leaveEntitlements.hoursEntitled(leaveType, super.getPeriod().getTo().toLocalDate());
     }
 }
