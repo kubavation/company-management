799a9a3dba7aaa0eff84aa1d3425619514e802a6
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-04 18:00:22 +0100

    DomainServicesRegistry

--- src/main/java/com/durys/jakub/companymanagement/commons/domain/DomainServicesRegistry.java
+++ src/main/java/com/durys/jakub/companymanagement/commons/domain/DomainServicesRegistry.java
@@ -1,5 +1,12 @@
 package com.durys.jakub.companymanagement.commons.domain;
 
-public interface DomainServicesRegistry {
-    <T> T instanceOf(Class<T> clazz);
+import com.durys.jakub.companymanagement.infrastructure.shared.registry.SpringDomainServicesRegistry;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DomainServicesRegistry {
+    public static <T> T instanceOf(Class<T> clazz) {
+        return SpringDomainServicesRegistry.instanceOf(clazz);
+    }
 }
--- src/main/java/com/durys/jakub/companymanagement/infrastructure/shared/registry/SpringDomainServicesRegistry.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/shared/registry/SpringDomainServicesRegistry.java
@@ -1,14 +1,12 @@
 package com.durys.jakub.companymanagement.infrastructure.shared.registry;
 
-import com.durys.jakub.companymanagement.commons.domain.DomainServicesRegistry;
 import lombok.RequiredArgsConstructor;
 
 
 @RequiredArgsConstructor
-public class SpringDomainServicesRegistry implements DomainServicesRegistry {
+public class SpringDomainServicesRegistry {
 
-    @Override
-    public <T> T instanceOf(Class<T> clazz) {
+    public static <T> T instanceOf(Class<T> clazz) {
         return SpringContext.getBean(clazz);
     }
 }
