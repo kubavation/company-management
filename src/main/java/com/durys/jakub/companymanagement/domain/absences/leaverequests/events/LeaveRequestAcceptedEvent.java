f849afaa911e110a123ad67b0b1e91791f65982d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-04 19:45:06 +0100

    change in leaveRequestAccepted event

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/events/LeaveRequestAcceptedEvent.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaverequests/events/LeaveRequestAcceptedEvent.java
@@ -1,11 +1,12 @@
 package com.durys.jakub.companymanagement.domain.absences.leaverequests.events;
 
 import com.durys.jakub.companymanagement.commons.events.DomainEvent;
-import com.durys.jakub.companymanagement.domain.absences.leaverequests.LeaveRequestPeriod;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
 
 
 @Getter
@@ -14,5 +15,5 @@ public class LeaveRequestAcceptedEvent implements DomainEvent {
 
     private final EmployeeId employeeId;
     private final LeaveRequestType leaveRequestType;
-    private final LeaveRequestPeriod period;
+    private final BigDecimal quantity;
 }
