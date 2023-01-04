4a78fe1640cf2f5573990ce77817c30f16d70ba3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-04 17:38:07 +0100

    changes in SpringDomainServicesRegistry usage of SpringContext

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/shared/registry/SpringDomainServicesRegistry.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/shared/registry/SpringDomainServicesRegistry.java
@@ -2,15 +2,13 @@ package com.durys.jakub.companymanagement.infrastructure.shared.registry;
 
 import com.durys.jakub.companymanagement.commons.domain.DomainServicesRegistry;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ListableBeanFactory;
+
 
 @RequiredArgsConstructor
 public class SpringDomainServicesRegistry implements DomainServicesRegistry {
 
-    private final ListableBeanFactory beanFactory;
-
     @Override
     public <T> T instanceOf(Class<T> clazz) {
-        return beanFactory.getBean(clazz);
+        return SpringContext.getBean(clazz);
     }
 }
