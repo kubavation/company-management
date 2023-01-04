b05ac6b875b634add508d5ba8e776cc52eba14dc
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-04 19:50:53 +0100

    init of recalculate method in leaveentitlements

--- src/main/java/com/durys/jakub/companymanagement/application/absences/leaveprivileges/LeavePrivilegesApplicationService.java
+++ src/main/java/com/durys/jakub/companymanagement/application/absences/leaveprivileges/LeavePrivilegesApplicationService.java
@@ -1,7 +1,9 @@
 package com.durys.jakub.companymanagement.application.absences.leaveprivileges;
 
 import com.durys.jakub.companymanagement.commons.events.DomainEventListener;
+import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeaveEntitlements;
 import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeaveEntitlementsRepository;
+import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.vo.LeaveType;
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.events.LeaveRequestAcceptedEvent;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,7 +16,15 @@ public class LeavePrivilegesApplicationService {
 
 
     @DomainEventListener
-    public void on(LeaveRequestAcceptedEvent event) {
-        //todo
+    public void onLeaveRequestAcceptation(LeaveRequestAcceptedEvent event) {
+       LeaveEntitlements leaveEntitlements = leaveEntitlementsRepository.load(event.getEmployeeId());
+
+       leaveEntitlements = leaveEntitlements
+               .recalculate(
+                       LeaveType.valueOf(event.getLeaveRequestType().getName()),
+                       event.getQuantity(),
+                       event.getTo().toLocalDate());
+
+       leaveEntitlementsRepository.save(leaveEntitlements);
     }
 }
