43c2b8e4f5caa03a058fc5a12b9c9e2b2234acd5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-07 21:11:07 +0100

    init of contractNumberProvider domainService

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractNumberProvider.java
@@ -0,0 +1,7 @@
+package com.durys.jakub.companymanagement.domain.contracts;
+
+import com.durys.jakub.companymanagement.commons.domain.DomainService;
+
+@DomainService
+public class ContractNumberProvider {
+}
