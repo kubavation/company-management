de32ef00ce4bf01fee0ed8d264f9ac2c91d4744d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-08 13:28:25 +0100

    change in validation in contract period -> abstract class

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/TrailEmployeeContract.java
+++
@@ -1,4 +0,0 @@
-package com.durys.jakub.companymanagement.domain.contracts.employment;
-
-public class TrailEmployeeContract implements EmploymentContract {
-}
--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/vo/ContractPeriod.java
@@ -8,40 +8,38 @@ import com.durys.jakub.companymanagement.domain.contracts.exception.InvalidContr
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class ContractPeriod {
+public abstract class ContractPeriod {
 
-    private final ContractPeriodType type;
     private final LocalDate from;
     private final LocalDate to;
 
-    ContractPeriod(ContractPeriodType type, LocalDate from, LocalDate to) {
+    public ContractPeriod(LocalDate from, LocalDate to) {
 
-        Objects.requireNonNull(type, "invalid period type param");
         Objects.requireNonNull(from, "invalid date from param");
-
-        validateContractPeriodByType(type, from, to);
-
-        this.type = type;
+        
         this.from = from;
         this.to = to;
+        validate();
     }
 
-    private void validateContractPeriodByType(ContractPeriodType type, LocalDate from, LocalDate to) {
+    abstract void validate();
 
-        if (ContractPeriodType.FIXED_TERM.equals(type)) {
-
-            if (Objects.isNull(to)) {
-                throw new ContractPeriodEndDateNotDefinedException();
-            }
-
-            if (!to.isAfter(from)) {
-                throw new InvalidContractPeriodException();
-            }
-        }
-
-        if (ContractPeriodType.PERMANENT.equals(type) && Objects.nonNull(to)) {
-            throw new ContractPeriodEndDateIncorrectlyDefined();
-        }
-
-    }
+//    private void validateContractPeriodByType(ContractPeriodType type, LocalDate from, LocalDate to) {
+//
+//        if (ContractPeriodType.FIXED_TERM.equals(type)) {
+//
+//            if (Objects.isNull(to)) {
+//                throw new ContractPeriodEndDateNotDefinedException();
+//            }
+//
+//            if (!to.isAfter(from)) {
+//                throw new InvalidContractPeriodException();
+//            }
+//        }
+//
+//        if (ContractPeriodType.PERMANENT.equals(type) && Objects.nonNull(to)) {
+//            throw new ContractPeriodEndDateIncorrectlyDefined();
+//        }
+//
+//    }
 }
