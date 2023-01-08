57f7c53feb3eda71aba7261d4c614abe0b9cb885
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-08 16:56:03 +0100

    basic factory of contracts

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractFactory.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractFactory.java
@@ -1,2 +1,15 @@
-package com.durys.jakub.companymanagement.domain.contracts;public class ContractFactory {
+package com.durys.jakub.companymanagement.domain.contracts;
+
+import com.durys.jakub.companymanagement.domain.contracts.employment.trail.TrailEmployeeContract;
+
+public class ContractFactory {
+
+
+    Contract instanceOf(ContractType type, Contract.Builder builder) {
+        if (ContractType.EMPLOYMENT.equals(type)) {
+            return new TrailEmployeeContract(builder);
+        }
+
+        throw new UnsupportedOperationException();
+    }
 }
