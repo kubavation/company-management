e59245ff410d410dc015bc09f35c0197db15540d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-08 14:08:00 +0100

    validation for TrailEmploymentContractPeriod

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/trail/TrailEmployeeContract.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/trail/TrailEmployeeContract.java
@@ -1,6 +1,18 @@
-package com.durys.jakub.companymanagement.domain.contracts.employment;
+package com.durys.jakub.companymanagement.domain.contracts.employment.trail;
 
 import com.durys.jakub.companymanagement.domain.contracts.Contract;
+import com.durys.jakub.companymanagement.domain.contracts.ContractId;
+import com.durys.jakub.companymanagement.domain.contracts.ContractNumber;
+import com.durys.jakub.companymanagement.domain.contracts.employment.EmploymentContract;
+import com.durys.jakub.companymanagement.domain.contracts.vo.ContractData;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+
 
 public class TrailEmployeeContract extends Contract implements EmploymentContract {
+
+    public TrailEmployeeContract(ContractId contractId, EmployeeId employeeId, ContractNumber contractNumber, ContractData contractData) {
+        super(contractId, employeeId, contractNumber, contractData);
+    }
+
+
 }
