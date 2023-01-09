b4f10b8018eeb0339706f8e035ded70aba60d121
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-09 19:48:00 +0100

    init of mandate contract

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/mandate/MandateContractPeriod.java
@@ -0,0 +1,28 @@
+package com.durys.jakub.companymanagement.domain.contracts.mandate;
+
+import com.durys.jakub.companymanagement.domain.contracts.exception.InvalidContractPeriodException;
+import com.durys.jakub.companymanagement.domain.contracts.vo.ContractPeriod;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public class MandateContractPeriod extends ContractPeriod {
+
+    public MandateContractPeriod(LocalDate from, LocalDate to) {
+        super(from, to);
+    }
+
+    @Override
+    protected void validate() {
+
+        Objects.requireNonNull(to(), "date to for mandate contract must be provided");
+
+        if (!to().isAfter(from())) {
+            throw new InvalidContractPeriodException();
+        }
+
+    }
+
+
+}
