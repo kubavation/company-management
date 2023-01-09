60935ad094b7a279508ff3955100dcffffb3efc8
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-09 18:19:07 +0100

    change in validation

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/fixedterm/FixedTermEmployeeContractPeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/fixedterm/FixedTermEmployeeContractPeriod.java
@@ -1,5 +1,6 @@
 package com.durys.jakub.companymanagement.domain.contracts.employment.fixedterm;
 
+import com.durys.jakub.companymanagement.domain.contracts.exception.InvalidContractPeriodException;
 import com.durys.jakub.companymanagement.domain.contracts.vo.ContractPeriod;
 
 import java.time.LocalDate;
@@ -14,5 +15,9 @@ public class FixedTermEmployeeContractPeriod extends ContractPeriod {
     @Override
     protected void validate() {
         Objects.requireNonNull(super.to(), "date to for fixed term employment contract must be provided");
+
+        if (!to().isAfter(from())) {
+            throw new InvalidContractPeriodException();
+        }
     }
 }
