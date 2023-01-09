ca290f64f8c63f5ccc8fe32162fd189447eafb70
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-09 20:19:33 +0100

    todo - contract number provider (test implementation)

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractNumberProvider.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractNumberProvider.java
@@ -2,10 +2,12 @@ package com.durys.jakub.companymanagement.domain.contracts;
 
 import com.durys.jakub.companymanagement.commons.domain.DomainService;
 
+import java.util.UUID;
+
 @DomainService
 public class ContractNumberProvider {
 
     ContractNumber generate(ContractType contractType) {
-        return new ContractNumber("todo");
+        return new ContractNumber(UUID.randomUUID().toString()); //todo explore domain
     }
 }
