b4d385a85ec848187b7fc909015cb4262559085d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-09 19:38:01 +0100

    notice period enum

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractNumber.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractNumber.java
@@ -1,6 +1,5 @@
 package com.durys.jakub.companymanagement.domain.contracts;
 
-import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
