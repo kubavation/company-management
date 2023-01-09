1abc5f580bd0852b57c82a9b0eaa79303b385073
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-09 20:12:27 +0100

    contract factory -> prepare method

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractFactory.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/ContractFactory.java
@@ -1,6 +1,9 @@
 package com.durys.jakub.companymanagement.domain.contracts;
 
+import com.durys.jakub.companymanagement.domain.contracts.employment.fixedterm.FixedTermEmployeeContract;
+import com.durys.jakub.companymanagement.domain.contracts.employment.permanent.PermanentEmployeeContract;
 import com.durys.jakub.companymanagement.domain.contracts.employment.trail.TrailEmployeeContract;
+import com.durys.jakub.companymanagement.domain.contracts.mandate.MandateContract;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -9,10 +12,12 @@ public class ContractFactory {
 
 
     public static Contract prepare(ContractType type, Contract.Builder builder) {
-        if (ContractType.EMPLOYMENT.equals(type)) {
-            return new TrailEmployeeContract(builder);
-        }
 
-        throw new UnsupportedOperationException();
+        return switch (type) {
+            case EMPLOYMENT_FIXED_TERM -> new FixedTermEmployeeContract(builder);
+            case EMPLOYMENT_PERMANENT -> new PermanentEmployeeContract(builder);
+            case EMPLOYMENT_TRAIL -> new TrailEmployeeContract(builder);
+            case MANDATE -> new MandateContract(builder);
+        };
     }
 }
