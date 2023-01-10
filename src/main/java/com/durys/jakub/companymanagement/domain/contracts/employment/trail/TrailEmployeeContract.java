8021946108d42900b331be82461371f3bd8a74ba
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-10 20:36:26 +0100

    implementation of noticePeriod in trail contract

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/trail/TrailEmployeeContract.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/trail/TrailEmployeeContract.java
@@ -2,7 +2,9 @@ package com.durys.jakub.companymanagement.domain.contracts.employment.trail;
 
 import com.durys.jakub.companymanagement.domain.contracts.Contract;
 import com.durys.jakub.companymanagement.domain.contracts.employment.EmploymentContract;
+import com.durys.jakub.companymanagement.domain.contracts.employment.NoticePeriod;
 
+import java.time.Period;
 
 
 public class TrailEmployeeContract extends Contract implements EmploymentContract {
@@ -12,6 +14,17 @@ public class TrailEmployeeContract extends Contract implements EmploymentContrac
     public TrailEmployeeContract(Contract.Builder builder) {
         super(builder.getContractId(), builder.getEmployeeId(), builder.getContractNumber(),
                 builder.getContractData(), new TrailEmploymentContractPeriod(builder.getFrom(), builder.getTo()));
+    }
+
+    //employment period based on contract duration
+    public NoticePeriod noticePeriod(Period employmentPeriod) {
+        if (employmentPeriod.getMonths() == 3) {
+            return NoticePeriod.TWO_WEEKS;
+        }
+        if (employmentPeriod.getDays() >= 14) {
+            return NoticePeriod.ONE_WEEK;
+        }
+        return NoticePeriod.THREE_DAYS;
     }
 
 }
