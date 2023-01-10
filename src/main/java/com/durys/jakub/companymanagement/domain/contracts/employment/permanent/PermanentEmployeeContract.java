12a2b6db7e7c0042951a5657333d42fcc4bdb06d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-10 20:30:05 +0100

    noticePeriod implementation in permanentContract

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/permanent/PermanentEmployeeContract.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/permanent/PermanentEmployeeContract.java
@@ -2,6 +2,9 @@ package com.durys.jakub.companymanagement.domain.contracts.employment.permanent;
 
 import com.durys.jakub.companymanagement.domain.contracts.Contract;
 import com.durys.jakub.companymanagement.domain.contracts.employment.EmploymentContract;
+import com.durys.jakub.companymanagement.domain.contracts.employment.NoticePeriod;
+
+import java.time.Period;
 
 
 public class PermanentEmployeeContract extends Contract implements EmploymentContract {
@@ -9,6 +12,16 @@ public class PermanentEmployeeContract extends Contract implements EmploymentCon
     public PermanentEmployeeContract(Builder builder) {
         super(builder.getContractId(), builder.getEmployeeId(), builder.getContractNumber(),
                 builder.getContractData(), new PermanentEmployeeContractPeriod(builder.getFrom(), builder.getTo()));
+    }
+
+    public NoticePeriod noticePeriod(Period employmentPeriod) {
+        if (employmentPeriod.getYears() >= 3) {
+            return NoticePeriod.THREE_MONTHS;
+        }
+        if (employmentPeriod.getMonths() >= 6) {
+            return NoticePeriod.ONE_MONTH;
+        }
+        return NoticePeriod.TWO_WEEKS;
     }
 
 }
