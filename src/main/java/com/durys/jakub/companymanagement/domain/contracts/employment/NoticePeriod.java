3755e1a8e18ea71eb8bc29c536bfc336334ab688
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-10 20:53:59 +0100

    additional fields in noticePeriod enum

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/NoticePeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/NoticePeriod.java
@@ -1,9 +1,19 @@
 package com.durys.jakub.companymanagement.domain.contracts.employment;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.temporal.ChronoUnit;
+
+@AllArgsConstructor
+@Getter
 public enum NoticePeriod {
     TWO_WEEKS,
     ONE_MONTH,
     THREE_MONTHS,
     THREE_DAYS,
-    ONE_WEEK
+    ONE_WEEK;
+
+    private final ChronoUnit unit;
+    private final Integer number;
 }
