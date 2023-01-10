7488b5605e2fa19eb31e1bfca5ba42bf6707155c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-10 19:08:41 +0100

    adttional currentOf method

--- src/main/java/com/durys/jakub/companymanagement/domain/employees/model/EmployeeRepository.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/employees/model/EmployeeRepository.java
@@ -1,8 +1,7 @@
 package com.durys.jakub.companymanagement.domain.employees.model;
 
-
-
 public interface EmployeeRepository {
+
     <T extends Employable> T load(EmployeeId employeeId);
 
     void save(Employee employee);
