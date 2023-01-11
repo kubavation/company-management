4e17b79e13985c807aad44fe4bb636b5509693c4
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-11 20:13:38 +0100

    impl of abract methods -> mandate contract

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/mandate/MandateContract.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/mandate/MandateContract.java
@@ -3,7 +3,11 @@ package com.durys.jakub.companymanagement.domain.contracts.mandate;
 import com.durys.jakub.companymanagement.domain.contracts.Contract;
 import com.durys.jakub.companymanagement.domain.contracts.ContractId;
 import com.durys.jakub.companymanagement.domain.contracts.employment.permanent.PermanentEmployeeContractPeriod;
+import com.durys.jakub.companymanagement.domain.contracts.vo.ContractPeriod;
 
+
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.UUID;
 
 public class MandateContract extends Contract {
@@ -11,5 +15,16 @@ public class MandateContract extends Contract {
     public MandateContract(Builder builder) {
         super(new ContractId(UUID.randomUUID()), builder.getEmployeeId(), builder.getContractNumber(),
                 builder.getContractData(), new PermanentEmployeeContractPeriod(builder.getFrom(), builder.getTo()));
+    }
+
+    @Override
+    protected ContractPeriod ofPeriod(LocalDate from, LocalDate to) {
+        return new PermanentEmployeeContractPeriod(from, to);
+    }
+
+    @Override
+    protected LocalDate calculateEndDate(LocalDate dateOfTermination, Period employmentPeriod) {
+        //todo explore domain logic
+        return dateOfTermination;
     }
 }
