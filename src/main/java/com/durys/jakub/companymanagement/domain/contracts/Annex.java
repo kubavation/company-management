e15451f2643104863261c847d1ba1056b9d91440
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-12 17:38:55 +0100

    added dateFrom field in annex

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/Annex.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/Annex.java
@@ -2,23 +2,24 @@ package com.durys.jakub.companymanagement.domain.contracts;
 
 import com.durys.jakub.companymanagement.domain.contracts.vo.ContractData;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 class Annex {
 
     private final AnnexId annexId;
-    private final ContractId contractId;
     private final ContractData contractData;
+    private final LocalDate dateFrom;
 
-    Annex(AnnexId annexId, ContractData contractData, ContractId contractId) {
+    Annex(AnnexId annexId, ContractData contractData, LocalDate dateFrom) {
         this.annexId = annexId;
         this.contractData = contractData;
-        this.contractId = contractId;
+        this.dateFrom = dateFrom;
     }
 
-     Annex(ContractData contractData, ContractId contractId) {
+     Annex(ContractData contractData, LocalDate dateFrom) {
         this.annexId = new AnnexId(UUID.randomUUID());
         this.contractData = contractData;
-        this.contractId = contractId;
+        this.dateFrom = dateFrom;
     }
 }
