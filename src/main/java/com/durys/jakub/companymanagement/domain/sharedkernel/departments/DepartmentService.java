9cdb48827eff1d4e308ca59e6012ea70ae221751
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-13 22:02:08 +0100

    departmentService interface

--- src/main/java/com/durys/jakub/companymanagement/domain/sharedkernel/departments/DepartmentService.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/sharedkernel/departments/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.durys.jakub.companymanagement.domain.sharedkernel.departments;
 
+import com.durys.jakub.companymanagement.domain.employees.model.vo.DepartmentId;
+
 public interface DepartmentService {
 
-
+    Department findById(DepartmentId departmentId);
 }
