6597cf3d602ebf7452cd9b8ef5898f97efba1b41
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-13 23:29:14 +0100

    todos

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/departments/RestDepartmentProvider.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/departments/RestDepartmentProvider.java
@@ -12,6 +12,8 @@ public class RestDepartmentProvider implements DepartmentProvider {
 
     private final WebClient webClient;
 
+    //todo converter
+
     @Override
     public Department find(DepartmentId departmentId) {
         return webClient.get().retrieve().toEntity(Department.class).block().getBody(); //todo
