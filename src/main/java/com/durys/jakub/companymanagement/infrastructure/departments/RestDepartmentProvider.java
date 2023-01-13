3ac077451aa91f47bec02f85ee894800dd025e34
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-13 23:22:42 +0100

    init impl of restDepartmentProvider

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/departments/RestDepartmentProvider.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/departments/RestDepartmentProvider.java
@@ -3,14 +3,18 @@ package com.durys.jakub.companymanagement.infrastructure.departments;
 import com.durys.jakub.companymanagement.domain.employees.model.vo.DepartmentId;
 import com.durys.jakub.companymanagement.domain.sharedkernel.departments.Department;
 import com.durys.jakub.companymanagement.domain.sharedkernel.departments.DepartmentProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
+@RequiredArgsConstructor
 public class RestDepartmentProvider implements DepartmentProvider {
 
+    private final WebClient webClient;
 
     @Override
     public Department find(DepartmentId departmentId) {
-        return null;
+        return webClient.get().retrieve().toEntity(Department.class).block().getBody(); //todo
     }
 }
