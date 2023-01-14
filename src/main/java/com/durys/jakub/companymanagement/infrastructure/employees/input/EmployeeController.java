1fc34ee82e1c63b82c810f788bc629d73ea63d8e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-14 14:47:19 +0100

    endpoint for createing employee

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/EmployeeController.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.durys.jakub.companymanagement.infrastructure.employees.input;
 
+import com.durys.jakub.companymanagement.application.employees.commands.EngageEmployeeCommand;
+import com.durys.jakub.companymanagement.cqrs.commands.CommandGateway;
 import com.durys.jakub.companymanagement.infrastructure.employees.input.model.EmployeeDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,7 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/employees")
 @RequiredArgsConstructor
-public class EmployeeController {
+class EmployeeController {
 
+    private final CommandGateway commandGateway;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void engageEmployee(@RequestBody EmployeeDTO employee) {
+        commandGateway.dispatch(
+                new EngageEmployeeCommand(
+                        employee.getFirstName(), employee.getLastName(),
+                        employee.getGender(), employee.getBirthdayDate(), employee.getStructureId()));
+    }
 
 }
--- src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/model/EmployeeDTO.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/model/EmployeeDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -11,5 +13,6 @@ public class EmployeeDTO {
     private String firstName;
     private String lastName;
     private String gender;
+    private LocalDate birthdayDate;
     private String structureId;
 }
