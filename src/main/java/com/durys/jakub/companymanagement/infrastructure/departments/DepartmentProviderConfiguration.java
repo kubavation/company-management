447f64a835716b58fc74c6758e281e711903d541
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-14 16:10:32 +0100

    added loadbalancing on webclient.builder

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/departments/DepartmentProviderConfiguration.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/departments/DepartmentProviderConfiguration.java
@@ -2,6 +2,7 @@ package com.durys.jakub.companymanagement.infrastructure.departments;
 
 import com.durys.jakub.companymanagement.domain.sharedkernel.departments.DepartmentProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,14 +10,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class DepartmentProviderConfiguration {
 
-
     @Bean
-    WebClient organizationStructureClient() {
-        return WebClient.builder().build();
+    @LoadBalanced
+    WebClient.Builder organizationStructureClient() {
+        return WebClient.builder()
+                .baseUrl("http://ORGANIZATION-STRUCTURE-SERVICE/api/organization-structure");
     }
 
     @Bean
-    DepartmentProvider restDepartmentProvider(@Qualifier("organizationStructureClient") WebClient webClient) {
-        return new RestDepartmentProvider(webClient);
+    DepartmentProvider restDepartmentProvider(@Qualifier("organizationStructureClient") WebClient.Builder webClient) {
+        return new RestDepartmentProvider(webClient.build());
     }
 }
