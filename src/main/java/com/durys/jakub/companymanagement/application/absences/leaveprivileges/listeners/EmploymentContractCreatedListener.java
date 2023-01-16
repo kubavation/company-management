c622efcdd4629f30da3a15d2ae853c0c3eeef43c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-16 21:10:10 +0100

    EmploymentContractCreatedListener - initial

---
+++ src/main/java/com/durys/jakub/companymanagement/application/absences/leaveprivileges/listeners/EmploymentContractCreatedListener.java
@@ -0,0 +1,27 @@
+package com.durys.jakub.companymanagement.application.absences.leaveprivileges.listeners;
+
+import com.durys.jakub.companymanagement.commons.events.DomainEventListener;
+import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeaveEntitlements;
+import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.LeaveEntitlementsRepository;
+import com.durys.jakub.companymanagement.domain.contracts.event.EmploymentContractCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class EmploymentContractCreatedListener {
+
+    private final LeaveEntitlementsRepository leaveEntitlementsRepository;
+
+    @Transactional
+    @DomainEventListener
+    public void onLeaveRequestAcceptation(EmploymentContractCreatedEvent event) {
+
+        LeaveEntitlements leaveEntitlements = leaveEntitlementsRepository.load(event.employeeId());
+        //todo
+
+        leaveEntitlementsRepository.save(leaveEntitlements);
+    }
+}
