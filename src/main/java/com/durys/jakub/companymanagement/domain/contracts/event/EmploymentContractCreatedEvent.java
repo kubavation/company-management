6cffe93337d6881ed811a7183a2e9f803f46cc27
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-16 21:04:37 +0100

    emiting event after creating permanent contract -todo

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/event/EmploymentContractCreatedEvent.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/event/EmploymentContractCreatedEvent.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.companymanagement.domain.contracts.event;
 
 import com.durys.jakub.companymanagement.commons.events.DomainEvent;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 
-public record EmploymentContractCreatedEvent(String employeeId) implements DomainEvent {
+public record EmploymentContractCreatedEvent(EmployeeId employeeId) implements DomainEvent {
 }
