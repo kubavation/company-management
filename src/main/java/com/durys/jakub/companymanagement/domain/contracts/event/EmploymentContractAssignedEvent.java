4df08b545ff001f14a034acc86bed7a3bc593321
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-17 17:37:17 +0100

    change to employmentContractAssigned event

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/event/EmploymentContractAssignedEvent.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/event/EmploymentContractAssignedEvent.java
@@ -3,5 +3,5 @@ package com.durys.jakub.companymanagement.domain.contracts.event;
 import com.durys.jakub.companymanagement.commons.events.DomainEvent;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 
-public record EmploymentContractCreatedEvent(EmployeeId employeeId) implements DomainEvent {
+public record EmploymentContractAssignedEvent(EmployeeId employeeId) implements DomainEvent {
 }
