b9efadbe96717dbf1f33bd2e51573bcb71ed5f98
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-17 21:47:47 +0100

    init implementation of employmentPeriod

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/EmploymentService.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/EmploymentService.java
@@ -1,14 +1,29 @@
 package com.durys.jakub.companymanagement.domain.contracts;
 
 import com.durys.jakub.companymanagement.commons.domain.DomainService;
+import com.durys.jakub.companymanagement.domain.contracts.employment.EmploymentContract;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import lombok.AllArgsConstructor;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Comparator;
+import java.util.Set;
 
 @DomainService
+@AllArgsConstructor
 public class EmploymentService {
 
-    public Period employmentPeriod(ContractId contractId) {
-        return Period.between(LocalDate.now(), LocalDate.now()); //todo
+    private final ContractRepository contractRepository;
+
+    public Integer employmentPeriod(EmployeeId employeeId) {
+        Set<Contract> contracts = contractRepository.loadBy(employeeId);
+        return contracts.stream()
+                .filter(contract -> contract instanceof EmploymentContract)
+                .sorted(Comparator.comparing(contract -> contract.period.from()))
+                .map(contract -> Period.between(contract.period.from(), contract.period.to()).getDays())
+                .reduce(0, Integer::sum)
+                .intValue();
     }
 }
