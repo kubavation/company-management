b9020549734b40a78ad7ee4fba28d2d2ad15a265
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-17 19:46:37 +0100

    type of graduation

--- src/main/java/com/durys/jakub/companymanagement/domain/employees/model/Graduation.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/employees/model/Graduation.java
@@ -6,7 +6,15 @@ import java.time.LocalDate;
 
 @AllArgsConstructor
 class Graduation {
+
+    public enum Type {
+        PRIMARY,
+        SECONDARY,
+        HIGHER
+    }
+
     private String schoolName;
     private LocalDate from;
     private LocalDate to;
+    private Type type;
 }
