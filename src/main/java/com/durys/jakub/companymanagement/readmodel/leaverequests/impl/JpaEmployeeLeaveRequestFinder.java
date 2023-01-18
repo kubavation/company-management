a676ca25714420051a3cc1a9a4a8fb501752cfdc
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-18 21:09:39 +0100

    JpaEmployeeLeaveRequestFinder

---
+++ src/main/java/com/durys/jakub/companymanagement/readmodel/leaverequests/impl/JpaEmployeeLeaveRequestFinder.java
@@ -0,0 +1,20 @@
+package com.durys.jakub.companymanagement.readmodel.leaverequests.impl;
+
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRequest;
+import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRequestFinder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JpaEmployeeLeaveRequestFinder implements EmployeeLeaveRequestFinder {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<EmployeeLeaveRequest> findAll(EmployeeId employeeId) {
+        return null; //todo
+    }
+}
