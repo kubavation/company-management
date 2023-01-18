c58e1d5ae301fddeb64dfd8e6e7b6d0ebb568a4b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-18 17:25:33 +0100

    additional length extractors in employment service

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/EmploymentService.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/EmploymentService.java
@@ -4,14 +4,18 @@ import com.durys.jakub.companymanagement.commons.domain.DomainService;
 import com.durys.jakub.companymanagement.domain.contracts.employment.EmploymentContract;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @DomainService
 @AllArgsConstructor
@@ -19,14 +23,32 @@ public class EmploymentService {
 
     private final ContractRepository contractRepository;
 
+    @RequiredArgsConstructor
+    public enum EmploymentPeriodMeasure {
+        MONTHS(days -> Period.ofDays(days).getMonths()),
+        YEARS(days -> Period.ofDays(days).getYears());
+
+        private final Function<Integer, Integer> extractor;
+    }
+
     public Integer employmentPeriod(EmployeeId employeeId) {
-        Set<Contract> contracts = contractRepository.loadBy(employeeId);
-        return contracts.stream()
-                .filter(contract -> contract instanceof EmploymentContract)
+        return loadContractDaysPeriod(employeeId)
+                .reduce(0, Integer::sum);
+    }
+
+    public Integer employmentPeriod(EmployeeId employeeId, EmploymentPeriodMeasure measure) {
+        return loadContractDaysPeriod(employeeId)
+                .map(measure.extractor)
+                .reduce(0, Integer::sum);
+    }
+
+
+    private Stream<Integer> loadContractDaysPeriod(EmployeeId employeeId) {
+        return contractRepository.loadBy(employeeId)
+                .stream()
+                .filter(EmploymentContract.class::isInstance)
                 .sorted(Comparator.comparing(contract -> contract.period.from()))
-                .map(this::toDays)
-                .reduce(0, Integer::sum)
-                .intValue();
+                .map(this::toDays);
     }
 
     private int toDays(Contract contract) {
@@ -34,4 +56,5 @@ public class EmploymentService {
                 contract.period.from(),
                 Objects.requireNonNullElse(contract.period.to(), LocalDate.now())).getDays();
     }
+
 }
