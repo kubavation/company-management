a6ea4514fedf8ce1d3f478ec3b87487f6c7c7c17
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-18 21:51:04 +0100

    submit leave request -endpoint

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/in_adapters/EmployeeLeaveRequestsController.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/in_adapters/EmployeeLeaveRequestsController.java
@@ -1,5 +1,10 @@
 package com.durys.jakub.companymanagement.infrastructure.absences.leaverequests.in_adapters;
 
+import com.durys.jakub.companymanagement.application.absences.leaverequests.commands.SubmitLeaveRequestCommand;
+import com.durys.jakub.companymanagement.cqrs.commands.CommandGateway;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.Applicant;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.ApplicantId;
+import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import com.durys.jakub.companymanagement.infrastructure.absences.leaverequests.dto.LeaveRequestDTO;
 import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRequest;
@@ -7,6 +12,8 @@ import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRe
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,10 +23,19 @@ import java.util.UUID;
 class EmployeeLeaveRequestsController {
 
     private final EmployeeLeaveRequestFinder finder;
+    private final CommandGateway commandGateway;
 
     @GetMapping
     List<EmployeeLeaveRequest> findEmployeeLeaveRequests(@PathVariable UUID employeeId) {
         return finder.findAll(EmployeeId.from(employeeId));
+    }
+
+    @PostMapping
+    void submitLeaveRequest(@PathVariable UUID employeeId, @RequestBody Object todo) {
+        //todo
+        commandGateway.dispatch(
+                new SubmitLeaveRequestCommand(
+                        employeeId, LeaveRequestType.AL, LocalDateTime.now(), LocalDateTime.now()));
     }
 
 }
