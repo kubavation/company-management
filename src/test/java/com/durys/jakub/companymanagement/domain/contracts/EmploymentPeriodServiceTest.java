6ecfe365f90554c2fe4d83b4528861b5262e708b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-18 19:08:01 +0100

    EmploymentPeriodServiceTest

--- src/test/java/com/durys/jakub/companymanagement/domain/contracts/EmploymentPeriodServiceTest.java
+++ src/test/java/com/durys/jakub/companymanagement/domain/contracts/EmploymentPeriodServiceTest.java
@@ -1,6 +1,8 @@
 package com.durys.jakub.companymanagement.domain.contracts;
 
+import com.durys.jakub.companymanagement.commons.events.DomainEvents;
 import com.durys.jakub.companymanagement.domain.contracts.employment.fixedterm.FixedTermEmployeeContract;
+import com.durys.jakub.companymanagement.domain.contracts.employment.permanent.PermanentEmployeeContract;
 import com.durys.jakub.companymanagement.domain.contracts.vo.ContractData;
 import com.durys.jakub.companymanagement.domain.contracts.vo.Position;
 import com.durys.jakub.companymanagement.domain.employees.model.Employee;
@@ -10,8 +12,11 @@ import com.durys.jakub.companymanagement.domain.employees.model.vo.PersonalData;
 import com.durys.jakub.companymanagement.domain.sharedkernel.TenantId;
 import com.durys.jakub.companymanagement.domain.sharedkernel.departments.Department;
 import com.durys.jakub.companymanagement.infrastructure.contracts.out.JdbcContractRepository;
+import com.durys.jakub.companymanagement.infrastructure.shared.registry.SpringContext;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -23,6 +28,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class EmploymentPeriodServiceTest {
 
 
@@ -59,7 +65,34 @@ class EmploymentPeriodServiceTest {
         assertEquals(expected, result);
     }
 
+    @Test
+    void employmentPeriod_withFixedAndPermanentContract_shouldReturnCorrectEmploymentPeriodInDays() {
 
+        EmployeeId employeeId = new EmployeeId(UUID.randomUUID());
+        Employee employee = employee(employeeId);
+        long expected = ChronoUnit.DAYS.between(
+                LocalDate.of(2022, 1, 1),
+                LocalDate.now()) + 1;
+
+        Set<Contract> contracts = Set.of(
+                new FixedTermEmployeeContract(
+                        Contract.Builder.instanceOf(ContractType.EMPLOYMENT_FIXED_TERM)
+                                .in(LocalDate.of(2022, 1, 1), LocalDate.of(2022, 1, 31))
+                                .withData(contractData())
+                                .assignTo(employee)),
+                new PermanentEmployeeContract(
+                        Contract.Builder.instanceOf(ContractType.EMPLOYMENT_FIXED_TERM)
+                                .from(LocalDate.of(2022, 2, 1))
+                                .withData(contractData())
+                                .assignTo(employee))
+        );
+
+
+        Mockito.when(contractRepository.loadBy(employeeId)).thenReturn(contracts);
+
+        Long result = employmentPeriodService.employmentPeriod(employeeId);
+        assertEquals(expected, result);
+    }
 
     private ContractData contractData() {
         return new ContractData("IT Expert", BigDecimal.valueOf(5000), 8,0);
