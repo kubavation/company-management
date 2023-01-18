b752aed27bbb90cea507856464a65ba707b97d6f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-18 21:46:45 +0100

    EmployeeLeaveRequestsController

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/in_adapters/EmployeeLeaveRequestsController.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/in_adapters/EmployeeLeaveRequestsController.java
@@ -1,22 +1,25 @@
 package com.durys.jakub.companymanagement.infrastructure.absences.leaverequests.in_adapters;
 
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import com.durys.jakub.companymanagement.infrastructure.absences.leaverequests.dto.LeaveRequestDTO;
+import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRequest;
 import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRequestFinder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/employees/{employeeId}/leave-requests")
 @RequiredArgsConstructor
 class EmployeeLeaveRequestsController {
 
     private final EmployeeLeaveRequestFinder finder;
 
-    @GetMapping("/employees/{employeeId}/leave-requestes/{leaveRequestId}")
-    LeaveRequestDTO findLeaveRequest(@PathVariable Long employeeId, @PathVariable UUID leaveRequestId) {
-       // return leaveRequestsApplicationService.loadLeaveRequest(new LeaveRequestId(leaveRequestId));
-        throw new UnsupportedOperationException();
+    @GetMapping
+    List<EmployeeLeaveRequest> findEmployeeLeaveRequests(@PathVariable UUID employeeId) {
+        return finder.findAll(EmployeeId.from(employeeId));
     }
 
 }
