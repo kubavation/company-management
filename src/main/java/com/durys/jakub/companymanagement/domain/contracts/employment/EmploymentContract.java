2afb9556e7a2776ddde3d8a7eb1c1fb1df8c2104
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-19 20:26:35 +0100

    cleaning

--- src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/EmploymentContract.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/contracts/employment/EmploymentContract.java
@@ -2,7 +2,6 @@ package com.durys.jakub.companymanagement.domain.contracts.employment;
 
 import com.durys.jakub.companymanagement.domain.employees.model.EmploymentPeriodService;
 
-import java.time.Period;
 
 public interface EmploymentContract {
     NoticePeriod noticePeriod(EmploymentPeriodService.EmploymentPeriod employmentPeriod);
