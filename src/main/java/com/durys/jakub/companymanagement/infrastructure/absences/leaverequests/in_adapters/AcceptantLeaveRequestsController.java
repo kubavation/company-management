9c88c210dbe6bf714b21bbb603a511b5a71f24a1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-01-20 18:52:31 +0100

    acceptantLeaveRequestController

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/in_adapters/AcceptantLeaveRequestsController.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/in_adapters/AcceptantLeaveRequestsController.java
@@ -1,14 +1,23 @@
 package com.durys.jakub.companymanagement.infrastructure.absences.leaverequests.in_adapters;
 
+import com.durys.jakub.companymanagement.application.absences.leaverequests.commands.AcceptLeaveRequestCommand;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
-@RequestMapping("/acceotants/{acceptantId}/leave-requests")
+@RequestMapping("/acceptants/{acceptantId}/leave-requests")
 @RequiredArgsConstructor
 class AcceptantLeaveRequestsController {
 
     private final CommandGateway commandGateway;
+
+
+    @PatchMapping("/{leaveRequestId}") //todo
+    public void accept(@PathVariable UUID acceptantId, @PathVariable UUID leaveRequestId) {
+        commandGateway.dispatch(new AcceptLeaveRequestCommand(leaveRequestId, acceptantId));
+    }
 
 }
