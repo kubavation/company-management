a51c5665a95a04fbaf8ffa3e098ce0f6e8b1ddbc
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-02 17:37:00 +0100

    ChangeEmployeePersonalDataCommand

---
+++ src/main/java/com/durys/jakub/companymanagement/application/employees/commands/ChangeEmployeePersonalDataCommand.java
@@ -0,0 +1,9 @@
+package com.durys.jakub.companymanagement.application.employees.commands;
+
+import com.durys.jakub.companymanagement.cqrs.commands.Command;
+
+import java.time.LocalDate;
+
+public record ChangeEmployeePersonalDataCommand(String employeeId, String firstName, String lastName,
+                                                String gender, LocalDate birthdayDate) implements Command {
+}
