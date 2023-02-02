faeebf76fad274d1ec9cf5e16194cf25c00b57d5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-02 17:44:49 +0100

    endpoint for changinng employee data

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/EmployeeController.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.durys.jakub.companymanagement.infrastructure.employees.input;
 
+import com.durys.jakub.companymanagement.application.employees.commands.ChangeEmployeePersonalDataCommand;
 import com.durys.jakub.companymanagement.application.employees.commands.EngageEmployeeCommand;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandGateway;
 import com.durys.jakub.companymanagement.infrastructure.employees.input.model.EmployeeDTO;
+import com.durys.jakub.companymanagement.infrastructure.employees.input.model.EmployeePersonalDataDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -29,6 +33,20 @@ class EmployeeController {
                         employee.getPersonalData().getGender(),
                         employee.getPersonalData().getBirthdayDate(),
                         employee.getDepartmentId()));
+    }
+
+    @PutMapping("/{employeeId}/personal-data")
+    @ResponseStatus(HttpStatus.OK)
+    public void changePersonalData(@PathVariable UUID employeeId, @RequestBody EmployeePersonalDataDTO personalData) {
+
+        log.info("change personal data for employeeId: {}", employeeId);
+
+        commandGateway.dispatch(
+                new ChangeEmployeePersonalDataCommand(
+                        employeeId, personalData.getFirstName(), personalData.getLastName(),
+                        personalData.getGender(), personalData.getBirthdayDate()
+                )
+        );
     }
 
 }
