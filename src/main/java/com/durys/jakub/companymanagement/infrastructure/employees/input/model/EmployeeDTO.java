f71d9022cb58a5d7849ad2186ee55f856dd443a0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-02 18:48:46 +0100

    documentation of employeeDTO

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/model/EmployeeDTO.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/employees/input/model/EmployeeDTO.java
@@ -1,5 +1,7 @@
 package com.durys.jakub.companymanagement.infrastructure.employees.input.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,13 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@ApiModel("Employee")
 public class EmployeeDTO {
 
+    @ApiModelProperty(value = "ID of department", example = "31aab9b6-a321-11ed-a8fc-0242ac120002")
     private String departmentId;
+    @ApiModelProperty(value = "Employee personal data")
     private EmployeePersonalDataDTO personalData;
+    @ApiModelProperty(value = "List of employee graduations")
     private List<GraduationDTO> graduations;
 }
