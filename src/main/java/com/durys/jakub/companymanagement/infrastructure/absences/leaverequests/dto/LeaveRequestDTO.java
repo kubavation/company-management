87dee1f4d39c3823f6aebffb244ec5593f15ca6e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-03 20:25:03 +0100

    desc of leave request dto

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/dto/LeaveRequestDTO.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/absences/leaverequests/dto/LeaveRequestDTO.java
@@ -1,6 +1,8 @@
 package com.durys.jakub.companymanagement.infrastructure.absences.leaverequests.dto;
 
 import com.durys.jakub.companymanagement.domain.absences.leaverequests.vo.LeaveRequestType;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ApiModel("Leave request")
 public class LeaveRequestDTO {
+    @ApiModelProperty(value = "Type of request", example = "CL")
     private LeaveRequestType requestType;
+    @ApiModelProperty(value = "Date from", example = "2022-01-01")
     private LocalDateTime from;
+    @ApiModelProperty(value = "Date to", example = "2022-01-01")
     private LocalDateTime to;
 }
