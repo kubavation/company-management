d30c4bd3779e5c386f2cef9ba7d2f9400b7a05b9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-04 11:14:39 +0100

    fix in tests

--- src/test/java/com/durys/jakub/companymanagement/application/employees/commands/handlers/EngageEmployeeCommandHandlerTest.java
+++ src/test/java/com/durys/jakub/companymanagement/application/employees/commands/handlers/EngageEmployeeCommandHandlerTest.java
@@ -1,35 +1,33 @@
 package com.durys.jakub.companymanagement.application.employees.commands.handlers;
 
 import com.durys.jakub.companymanagement.application.employees.commands.EngageEmployeeCommand;
+import com.durys.jakub.companymanagement.commons.IdentityProvider;
 import com.durys.jakub.companymanagement.domain.employees.model.Employee;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeRepository;
 import com.durys.jakub.companymanagement.domain.employees.model.vo.DepartmentId;
 import com.durys.jakub.companymanagement.domain.sharedkernel.departments.Department;
+import com.durys.jakub.companymanagement.domain.sharedkernel.departments.DepartmentProvider;
 import com.durys.jakub.companymanagement.infrastructure.departments.RestDepartmentProvider;
 import com.durys.jakub.companymanagement.infrastructure.employees.HashMapEmployeeRepository;
 import com.durys.jakub.companymanagement.infrastructure.shared.identity.UUIDIdentityProvider;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 class EngageEmployeeCommandHandlerTest {
 
      EmployeeRepository employeeRepository = new HashMapEmployeeRepository();
-     @Mock
-     UUIDIdentityProvider identityProvider;
-     @Mock
-     RestDepartmentProvider departmentProvider;
+     IdentityProvider identityProvider = Mockito.mock(UUIDIdentityProvider.class);
+     DepartmentProvider departmentProvider = Mockito.mock(RestDepartmentProvider.class);
 
      EngageEmployeeCommandHandler commandHandler = new EngageEmployeeCommandHandler(employeeRepository, identityProvider, departmentProvider);
 
@@ -38,18 +36,14 @@ class EngageEmployeeCommandHandlerTest {
 
          UUID expectedId = UUID.randomUUID();
          EngageEmployeeCommand command = new EngageEmployeeCommand("John", "Doe", "MAN", LocalDate.now(), "123");
-         
+
          when(departmentProvider.find(new DepartmentId("123")))
                  .thenReturn(new Department(new DepartmentId("123"), "Dep1", "D1", "/D1"));
-
          when(identityProvider.nextId()).thenReturn(expectedId);
-
 
          commandHandler.handle(command);
 
          Employee engagedEmployee = employeeRepository.load(new EmployeeId(expectedId));
-
-         verify(employeeRepository).save(Mockito.any(Employee.class));
          assertEquals(expectedId, engagedEmployee.employeeId().value());
      }
 
