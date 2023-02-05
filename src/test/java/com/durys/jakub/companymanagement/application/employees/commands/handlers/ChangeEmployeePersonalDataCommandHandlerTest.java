6790f15f6b3d080180ff35ccc2923c65090d9575
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-05 15:02:54 +0100

    init ChangeEmployeePersonalDataCommandHandlerTest

---
+++ src/test/java/com/durys/jakub/companymanagement/application/employees/commands/handlers/ChangeEmployeePersonalDataCommandHandlerTest.java
@@ -0,0 +1,7 @@
+package com.durys.jakub.companymanagement.application.employees.commands.handlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChangeEmployeePersonalDataCommandHandlerTest {
+
+}
