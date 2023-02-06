1bf63b9d361b233a9ce0c6d699c3bde8360c5a67
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-06 19:53:56 +0100

    finals

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequest.java
@@ -2,7 +2,11 @@ package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 public class WorkingTimeRequest {
     private final WorkingTimeRequestId requestId;
+    private final LocalDate atDay;
+    private final WorkingTimeRequestPeriod period;
 }
