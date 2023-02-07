a0c75d5f6f7fadb027aa26bd6fadf31bfb3b58ec
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-07 17:59:54 +0100

    fix

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequestPeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequestPeriod.java
@@ -2,11 +2,10 @@ package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
 import lombok.AllArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 @AllArgsConstructor
 class WorkingTimeRequestPeriod {
     private LocalTime from;
-    private LocalDate to;
+    private LocalTime to;
 }
