e30be49bcb77fb4d1c391812255793de69282ac6
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-09 20:30:57 +0100

    author id field

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequest.java
@@ -1,5 +1,6 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
@@ -7,6 +8,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class WorkingTimeRequest {
     private final WorkingTimeRequestId requestId;
+    private final EmployeeId authorId;
     private final LocalDate atDay;
     private final WorkingTimeRequestPeriod period;
     private final WorkingTimeRequestType type;
