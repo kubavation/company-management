b422b2f563d194488fb336ee20198192e13b4002
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-13 20:06:33 +0100

    events field in workday aggregate

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/WorkDay.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/WorkDay.java
@@ -3,15 +3,19 @@ package com.durys.jakub.companymanagement.domain.workingtime;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 public class WorkDay {
     private final WorkDayId id;
     private final EmployeeId employeeId;
     private final LocalDate day;
+    private List<WorkDayEvent> events;
 
     public WorkDay(WorkDayId id, EmployeeId employeeId, LocalDate day) {
         this.id = id;
         this.employeeId = employeeId;
         this.day = day;
+        this.events = Collections.emptyList();
     }
 }
