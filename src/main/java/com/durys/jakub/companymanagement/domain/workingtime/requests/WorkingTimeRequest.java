b7f0c8d325edb79c23edab337b415f7bf2661de9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-14 17:35:19 +0100

    status field in workingTimeRequest

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequest.java
@@ -1,14 +1,22 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
-import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 
-@AllArgsConstructor
 public abstract class WorkingTimeRequest {
     private final WorkingTimeRequestId requestId;
     private final EmployeeId authorId;
     private final LocalDate atDay;
     private final WorkingTimeRequestPeriod period;
+    private WorkingTimeRequestStatus status;
+
+    public WorkingTimeRequest(WorkingTimeRequestId requestId, EmployeeId authorId, LocalDate atDay,
+                              WorkingTimeRequestPeriod period, WorkingTimeRequestStatus status) {
+        this.requestId = requestId;
+        this.authorId = authorId;
+        this.atDay = atDay;
+        this.period = period;
+        this.status = status;
+    }
 }
