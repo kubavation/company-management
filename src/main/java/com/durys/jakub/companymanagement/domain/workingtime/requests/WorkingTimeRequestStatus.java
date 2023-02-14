98f2d131d51b6f13b1c7bf19a037abfd98259f75
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-14 16:37:35 +0100

    WorkingTimeRequestStatus

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequestStatus.java
@@ -0,0 +1,9 @@
+package com.durys.jakub.companymanagement.domain.workingtime.requests;
+
+public enum WorkingTimeRequestStatus {
+    SUBMITTED,
+    SENT_FOR_ACCEPTATION,
+    ACCEPTED,
+    REJECTED,
+    CANCELLED
+}
