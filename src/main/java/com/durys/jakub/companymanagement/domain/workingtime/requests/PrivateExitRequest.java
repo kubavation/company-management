3263e27da462ed2cd43a3cc9fc783276efda246a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-14 19:31:02 +0100

    removed unused imports

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/PrivateExitRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/PrivateExitRequest.java
@@ -1,11 +1,6 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
-import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequest;
-import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestId;
-import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestPeriod;
-import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestStatus;
-
 import java.time.LocalDate;
 
 public class PrivateExitRequest extends WorkingTimeRequest {
@@ -14,9 +9,5 @@ public class PrivateExitRequest extends WorkingTimeRequest {
                               WorkingTimeRequestPeriod period, WorkingTimeRequestStatus status) {
         super(requestId, authorId, atDay, period, status);
     }
-
-    @Override
-    public WorkingTimeRequest submit(WorkingTimeRequest.Submittable workInProgress) {
-        return null;
-    }
+    
 }
