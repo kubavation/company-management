4dc6f7db28b477729eff3de7da14ed0dbb142fb5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-15 19:57:14 +0100

    init of work schedule domain

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/workschedule/WorkSchedule.java
@@ -0,0 +1,2 @@
+package com.durys.jakub.companymanagement.domain.workschedule;public class WorkSchedule {
+}
---
+++ src/main/java/com/durys/jakub/companymanagement/domain/workschedule/WorkScheduleId.java
@@ -0,0 +1,5 @@
+package com.durys.jakub.companymanagement.domain.workschedule;
+
+import java.util.UUID;
+
+public record WorkScheduleId(UUID val) {}
