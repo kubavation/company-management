e8ef0e398c2d8495fb369a8180b2c2ae5b982097
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-16 17:42:58 +0100

    some fixes

--- src/main/java/com/durys/jakub/companymanagement/application/workingtime/listeners/WorkingTimeRequestAcceptedListener.java
+++ src/main/java/com/durys/jakub/companymanagement/application/workingtime/listeners/WorkingTimeRequestAcceptedListener.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.companymanagement.application.workingtime.listeners;
 
 import com.durys.jakub.companymanagement.commons.events.DomainEventListener;
+import com.durys.jakub.companymanagement.domain.workingtime.WorkDayRepository;
 import com.durys.jakub.companymanagement.domain.workingtime.event.WorkingTimeRequestAcceptedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,8 @@ import javax.transaction.Transactional;
 @Component
 @RequiredArgsConstructor
 public class WorkingTimeRequestAcceptedListener {
+
+    private final WorkDayRepository workDayRepository;
 
     @Transactional
     @DomainEventListener
--- src/main/java/com/durys/jakub/companymanagement/domain/workschedule/WorkScheduleId.java
+++
@@ -1,5 +0,0 @@
-package com.durys.jakub.companymanagement.domain.workschedule;
-
-import java.util.UUID;
-
-public record WorkScheduleId(UUID val) {}
