9c02f66f0fd7a70861abd8ed7b1a464e9ade88d8
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-22 19:22:27 +0100

    dayType in workSchedule

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/schedule/WorkingSchedule.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/schedule/WorkingSchedule.java
@@ -12,21 +12,23 @@ public class WorkingSchedule {
     private final LocalDate day;
     private final LocalTime from;
     private final LocalTime to;
+    private final WorkDayType dayType;
 
-    public WorkingSchedule(WorkingScheduleId workingScheduleId, EmployeeId employeeId, LocalDate day, LocalTime from, LocalTime to) {
+    public WorkingSchedule(WorkingScheduleId workingScheduleId, EmployeeId employeeId, WorkDayType dayType, LocalDate day, LocalTime from, LocalTime to) {
         this.workingScheduleId = workingScheduleId;
         this.employeeId = employeeId;
+        this.dayType = dayType;
         this.day = day;
         this.from = from;
         this.to = to;
     }
 
     public static WorkingSchedule dayOff(WorkingScheduleId workingScheduleId, EmployeeId employeeId, LocalDate day) {
-        return new WorkingSchedule(workingScheduleId, employeeId, day, null, null);
+        return new WorkingSchedule(workingScheduleId, employeeId, WorkDayType.DAY_OFF, day, null, null);
     }
 
     public static WorkingSchedule workingDay(WorkingScheduleId workingScheduleId, EmployeeId employeeId, LocalDate day, LocalTime from, LocalTime to) {
-        return new WorkingSchedule(workingScheduleId, employeeId, day, from, to);
+        return new WorkingSchedule(workingScheduleId, employeeId, WorkDayType.SCHEDULE, day, from, to);
     }
 
 }
