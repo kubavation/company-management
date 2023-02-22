d00cba0d1d28f5d8987db1e468f30c1c76ecbf6b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-22 18:11:01 +0100

    changes in packages in WorkDayType

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/WorkDay.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/WorkDay.java
@@ -3,6 +3,7 @@ package com.durys.jakub.companymanagement.domain.workingtime;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import com.durys.jakub.companymanagement.domain.workingtime.exception.InvalidWorkDayEventException;
 import com.durys.jakub.companymanagement.domain.workingtime.exception.WorkDayEventAlreadyAssignedInPeriodException;
+import com.durys.jakub.companymanagement.domain.workingtime.schedule.WorkDayType;
 import lombok.NonNull;
 
 import java.time.LocalDate;
