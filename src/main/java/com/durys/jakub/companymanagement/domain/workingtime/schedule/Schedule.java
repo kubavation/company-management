5f0cdaa4d6825ce72f3278dc2e4bf5354ff95b01
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-27 16:20:45 +0100

    schedule - additional constructor - refactor

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/schedule/Schedule.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/schedule/Schedule.java
@@ -15,7 +15,7 @@ public class Schedule {
     private final WorkDayType dayType;
 
     private Schedule(@NonNull ScheduleId scheduleId, @NonNull EmployeeId employeeId, @NonNull WorkDayType dayType,
-                    @NonNull LocalDate day, LocalTime from, LocalTime to) {
+                    @NonNull LocalDate day, @NonNull LocalTime from, @NonNull LocalTime to) {
         this.scheduleId = scheduleId;
         this.employeeId = employeeId;
         this.dayType = dayType;
@@ -23,8 +23,16 @@ public class Schedule {
         this.period = new SchedulePeriod(from, to);
     }
 
+    private Schedule(@NonNull ScheduleId scheduleId, @NonNull EmployeeId employeeId, @NonNull WorkDayType dayType, @NonNull LocalDate day) {
+        this.scheduleId = scheduleId;
+        this.employeeId = employeeId;
+        this.dayType = dayType;
+        this.day = day;
+        this.period = null;
+    }
+
     public static Schedule dayOff(ScheduleId scheduleId, EmployeeId employeeId, LocalDate day) {
-        return new Schedule(scheduleId, employeeId, WorkDayType.DAY_OFF, day, null, null);
+        return new Schedule(scheduleId, employeeId, WorkDayType.DAY_OFF, day);
     }
 
     public static Schedule workingDay(ScheduleId scheduleId, EmployeeId employeeId, LocalDate day, LocalTime from, LocalTime to) {
