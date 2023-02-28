1bfcb90bc18da2de164f9b56a83d76aabc17ff60
kubavation <37585627+kubavation@users.noreply.github.com>
2023-02-28 19:58:27 +0100

    fix in jdcbSceduleRepo

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/JdbcScheduleRepository.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/JdbcScheduleRepository.java
@@ -3,8 +3,13 @@ package com.durys.jakub.companymanagement.infrastructure.workingtime;
 import com.durys.jakub.companymanagement.domain.workingtime.Schedule;
 import com.durys.jakub.companymanagement.domain.workingtime.ScheduleId;
 import com.durys.jakub.companymanagement.domain.workingtime.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@RequiredArgsConstructor
 public class JdbcScheduleRepository implements ScheduleRepository {
+
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public Schedule load(ScheduleId id) {
