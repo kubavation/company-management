fffd17b7fc870166a15b4f73754a46c65d974dc0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-01 17:20:00 +0100

    durationOfWorkDayEventInBillingPeriod init method in scheduleService

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/ScheduleService.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/ScheduleService.java
@@ -3,9 +3,18 @@ package com.durys.jakub.companymanagement.domain.workingtime;
 import com.durys.jakub.companymanagement.commons.domain.DomainService;
 import lombok.RequiredArgsConstructor;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
 @DomainService
 @RequiredArgsConstructor
 public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
+
+
+    Duration durationOfWorkDayEventInBillingPeriod(WorkDayEventType eventType, LocalDate atDay, BillingPeriod billingPeriod) {
+        //todo
+        return Duration.ofDays(1);
+    }
 }
