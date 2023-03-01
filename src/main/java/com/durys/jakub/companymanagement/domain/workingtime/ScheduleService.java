f7b7913ba3548328fece2eecc390fe032436ec29
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-01 17:48:22 +0100

    additional validation

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/ScheduleService.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/ScheduleService.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.companymanagement.domain.workingtime;
 
 import com.durys.jakub.companymanagement.commons.domain.DomainService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import java.time.Duration;
@@ -13,7 +14,8 @@ public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
 
-    Duration durationOfWorkDayEventInBillingPeriod(WorkDayEventType eventType, LocalDate atDay, BillingPeriod billingPeriod) {
+    Duration durationOfWorkDayEventInBillingPeriod(@NonNull WorkDayEventType eventType, @NonNull LocalDate atDay,
+                                                   @NonNull BillingPeriod billingPeriod) {
         //todo
         return Duration.ofDays(1);
     }
