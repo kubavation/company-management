3cdaa8d1179dd6fb27fb173204b70e55cf009a9f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-01 17:50:42 +0100

    fix in durationOfWorkDayEventInBillingPeriod -> added employeeId param

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/ScheduleService.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/ScheduleService.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.companymanagement.domain.workingtime;
 
 import com.durys.jakub.companymanagement.commons.domain.DomainService;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -14,8 +15,8 @@ public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
 
-    Duration durationOfWorkDayEventInBillingPeriod(@NonNull WorkDayEventType eventType, @NonNull LocalDate atDay,
-                                                   @NonNull BillingPeriod billingPeriod) {
+    Duration durationOfWorkDayEventInBillingPeriod(@NonNull EmployeeId employeeId, @NonNull WorkDayEventType eventType,
+                                                   @NonNull LocalDate atDay, @NonNull BillingPeriod billingPeriod) {
         //todo
         return Duration.ofDays(1);
     }
