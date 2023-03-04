5a311b5a0f9c4e86cf0e3ff4d75eddc17d0fcad4
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-04 15:54:37 +0100

    scheduleId in WorkingTimeRequestAcceptedEvent

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/event/WorkingTimeRequestAcceptedEvent.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/event/WorkingTimeRequestAcceptedEvent.java
@@ -1,13 +1,11 @@
 package com.durys.jakub.companymanagement.domain.workingtime.event;
 
 import com.durys.jakub.companymanagement.commons.events.DomainEvent;
-import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import com.durys.jakub.companymanagement.domain.workingtime.ScheduleId;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestPeriod;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestType;
 
-import java.time.LocalDate;
 
-
-public record WorkingTimeRequestAcceptedEvent(EmployeeId employeeId, WorkingTimeRequestType type,
-                                              LocalDate atDay, WorkingTimeRequestPeriod period) implements DomainEvent {
+public record WorkingTimeRequestAcceptedEvent(ScheduleId scheduleId, WorkingTimeRequestType type,
+                                              WorkingTimeRequestPeriod period) implements DomainEvent {
 }
