e04fd45fd8eabe8d48446c73521050bc867e8184
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-08 19:57:48 +0100

    AcceptWorkingTimeRequestCommandHandler

--- src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/handlers/AcceptWorkingTimeRequestCommandHandler.java
+++ src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/handlers/AcceptWorkingTimeRequestCommandHandler.java
@@ -1,29 +1,23 @@
 package com.durys.jakub.companymanagement.application.workingtime.requests.handlers;
 
-import com.durys.jakub.companymanagement.application.workingtime.requests.SendWorkingTimeRequestForAcceptationCommand;
+import com.durys.jakub.companymanagement.application.workingtime.requests.AcceptWorkingTimeRequestCommand;
+import com.durys.jakub.companymanagement.application.workingtime.requests.CancelWorkingTimeRequestCommand;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandler;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandling;
-import com.durys.jakub.companymanagement.domain.employees.model.Employee;
-import com.durys.jakub.companymanagement.domain.employees.model.EmployeeRepository;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequest;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestRepository;
 import lombok.RequiredArgsConstructor;
 
 @CommandHandling
 @RequiredArgsConstructor
-public class SendWorkingTimeRequestForAcceptationCommandHandler implements CommandHandler<SendWorkingTimeRequestForAcceptationCommand> {
+public class AcceptWorkingTimeRequestCommandHandler implements CommandHandler<AcceptWorkingTimeRequestCommand> {
 
     private final WorkingTimeRequestRepository workingTimeRequestRepository;
-    private final EmployeeRepository employeeRepository;
 
     @Override
-    public void handle(SendWorkingTimeRequestForAcceptationCommand command) {
-
+    public void handle(AcceptWorkingTimeRequestCommand command) {
         WorkingTimeRequest request = workingTimeRequestRepository.load(command.requestId());
-        Employee acceptant = employeeRepository.load(command.acceptantId());
-
-        request.sendTo(acceptant);
-
+        request.markAsAccepted();
         workingTimeRequestRepository.save(request);
     }
 }
