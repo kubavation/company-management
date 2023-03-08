e22ea3da298b0700d26693ab1ae3cf87988ba4b7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-08 19:43:17 +0100

    CancelWorkingTimeRequestCommandHandler

--- src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/handlers/CancelWorkingTimeRequestCommandHandler.java
+++ src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/handlers/CancelWorkingTimeRequestCommandHandler.java
@@ -1,19 +1,22 @@
 package com.durys.jakub.companymanagement.application.workingtime.requests.handlers;
 
-import com.durys.jakub.companymanagement.application.absences.leaverequests.commands.CancelLeaveRequestCommand;
+import com.durys.jakub.companymanagement.application.workingtime.requests.CancelWorkingTimeRequestCommand;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandler;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandling;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequest;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestRepository;
 import lombok.RequiredArgsConstructor;
 
 @CommandHandling
 @RequiredArgsConstructor
-public class CancelWorkingTimeRequestCommandHandler implements CommandHandler<CancelLeaveRequestCommand> {
+public class CancelWorkingTimeRequestCommandHandler implements CommandHandler<CancelWorkingTimeRequestCommand> {
 
     private final WorkingTimeRequestRepository workingTimeRequestRepository;
 
     @Override
-    public void handle(CancelLeaveRequestCommand cancelLeaveRequestCommand) {
-        //todo
+    public void handle(CancelWorkingTimeRequestCommand command) {
+        WorkingTimeRequest request = workingTimeRequestRepository.load(command.requestId());
+        request.markAsCancelled();
+        workingTimeRequestRepository.save(request);
     }
 }
