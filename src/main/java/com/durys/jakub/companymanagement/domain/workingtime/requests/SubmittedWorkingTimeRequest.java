0aee2b6d534b831cf8c0e0468acb37df7eca896d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-10 19:03:47 +0100

    additional validation in submittedWorkingTime reques

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SubmittedWorkingTimeRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SubmittedWorkingTimeRequest.java
@@ -1,10 +1,13 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
+import lombok.NonNull;
+
 public class SubmittedWorkingTimeRequest {
 
     private final WorkingTimeRequest request;
 
-    public SubmittedWorkingTimeRequest(WorkingTimeRequest request) {
+    public SubmittedWorkingTimeRequest(@NonNull WorkingTimeRequest request) {
         this.request = request;
     }
+
 }
