5a823eeeffb65da6be6d7a5b616905ff924545e0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-10 19:11:11 +0100

    acceptantId field in SentForAcceptationWorkingTimeRequest

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SentForAcceptationWorkingTimeRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SentForAcceptationWorkingTimeRequest.java
@@ -1,12 +1,15 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.NonNull;
 
 public class SentForAcceptationWorkingTimeRequest {
 
     private final WorkingTimeRequest request;
+    private final EmployeeId acceptantId;
 
-    public SentForAcceptationWorkingTimeRequest(@NonNull WorkingTimeRequest request) {
+    public SentForAcceptationWorkingTimeRequest(@NonNull WorkingTimeRequest request, @NonNull EmployeeId acceptantId) {
         this.request = request;
+        this.acceptantId = acceptantId;
     }
 }
