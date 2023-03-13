d8618bdd55c2cb330034db2a587aed45657d6522
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-13 19:52:32 +0100

    init of WorkingTimeRequestFactory

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequestFactory.java
@@ -0,0 +1,16 @@
+package com.durys.jakub.companymanagement.domain.workingtime.requests;
+
+import lombok.NonNull;
+
+public class WorkingTimeRequestFactory {
+
+    public static RequestInWorkflow instanceFrom(@NonNull WorkingTimeRequest request, @NonNull WorkingTimeRequestStatus status) {
+        return switch (status) {
+            case SUBMITTED -> new SubmittedWorkingTimeRequest(request);
+            case ACCEPTED -> new AcceptedWorkingTimeRequest(request);
+            case CANCELLED -> new CancelledWorkingTimeRequest(request);
+            case REJECTED -> new RejectedWorkingTimeRequest(request);
+            case SENT_FOR_ACCEPTATION -> new SentForAcceptationWorkingTimeRequest(request);
+        }
+    }
+}
