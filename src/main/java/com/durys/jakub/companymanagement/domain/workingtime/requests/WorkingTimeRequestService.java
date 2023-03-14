d9e9118d990d8cf34c0224242d40f8119950cea1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-14 20:43:47 +0100

    init of WorkingTimeRequestService

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequestService.java
@@ -0,0 +1,8 @@
+package com.durys.jakub.companymanagement.domain.workingtime.requests;
+
+import com.durys.jakub.companymanagement.commons.domain.DomainService;
+
+@DomainService
+public class WorkingTimeRequestService {
+
+}
