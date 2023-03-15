8328d5081a25c3c0656ceb2a4bb933353cc3f77d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-15 19:57:32 +0100

    submit implementation

--- src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/handlers/SubmitWorkingTimeRequestCommandHandler.java
+++ src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/handlers/SubmitWorkingTimeRequestCommandHandler.java
@@ -3,8 +3,13 @@ package com.durys.jakub.companymanagement.application.workingtime.requests.handl
 import com.durys.jakub.companymanagement.application.workingtime.requests.SubmitWorkingTimeRequestCommand;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandler;
 import com.durys.jakub.companymanagement.cqrs.commands.CommandHandling;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.SubmittedWorkingTimeRequest;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequest;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestId;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @CommandHandling
 @RequiredArgsConstructor
@@ -15,5 +20,15 @@ public class SubmitWorkingTimeRequestCommandHandler implements CommandHandler<Su
     @Override
     public void handle(SubmitWorkingTimeRequestCommand command) {
 
+        SubmittedWorkingTimeRequest request = WorkingTimeRequest.builder()
+                    .id(new WorkingTimeRequestId(UUID.randomUUID()))
+                    .ofType(command.type())
+                    .author(command.employeeId())
+                    .at(command.day())
+                        .from(command.from())
+                        .to(command.to())
+                    .submit();
+
+        workingTimeRequestRepository.save(request);
     }
 }
