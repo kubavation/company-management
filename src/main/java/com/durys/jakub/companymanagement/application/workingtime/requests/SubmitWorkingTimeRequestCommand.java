cadaf807ced306f963c1bb13ab426f23ef38c07b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-15 18:51:09 +0100

    SubmitWorkingTimeRequestCommand

--- src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/SubmitWorkingTimeRequestCommand.java
+++ src/main/java/com/durys/jakub/companymanagement/application/workingtime/requests/SubmitWorkingTimeRequestCommand.java
@@ -1,7 +1,12 @@
 package com.durys.jakub.companymanagement.application.workingtime.requests;
 
 import com.durys.jakub.companymanagement.cqrs.commands.Command;
-import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestId;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestType;
 
-public record CancelWorkingTimeRequestCommand(WorkingTimeRequestId requestId) implements Command {
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record SubmitWorkingTimeRequestCommand(EmployeeId employeeId, LocalDate day,  WorkingTimeRequestType type,
+                                              LocalTime from, LocalTime to) implements Command {
 }
