05f1d700cd5a8ded62b1a5eb2c1b78beb10f3e62
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-20 21:02:11 +0100

    basic query for findEmployeesWithPersonalData

--- src/main/java/com/durys/jakub/companymanagement/readmodel/employees/impl/JdbcEmployeeFinder.java
+++ src/main/java/com/durys/jakub/companymanagement/readmodel/employees/impl/JdbcEmployeeFinder.java
@@ -5,7 +5,10 @@ import com.durys.jakub.companymanagement.readmodel.employees.EmployeeWithPersona
 import com.durys.jakub.companymanagement.readmodel.shared.SearchCriteria;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,6 +19,23 @@ public class JdbcEmployeeFinder implements EmployeeFinder {
 
     @Override
     public List<EmployeeWithPersonalData> findEmployeesWithPersonalData(SearchCriteria criteria) {
-        return Collections.emptyList();
+
+        String sql = "SELECT e.id employeeId, pd.first_name firstName, pd.last_name lastName," +
+                "pd.gender gender, pd.birthday_date birthdayDate," +
+                "d.department_id departmentId, d.department_name departmentName," +
+                "d.department_shortcut departmentShortcut, d.department_path departmentPath" +
+                " FROM CM_EMPLOYEE e" +
+                " INNER JOIN CM_EMPLOYEE_PERSONAL_DATA pd on pd.employee_id = e.id " +
+                " LEFT JOIN CM_EMPLOYEE_DEPARTMENT d on d.id = e.department_id ";
+
+       return jdbcTemplate.query(sql, new EmployeeWithPersonalDataRowMapper());
+    }
+
+    private class EmployeeWithPersonalDataRowMapper implements RowMapper<EmployeeWithPersonalData> {
+
+        @Override
+        public EmployeeWithPersonalData mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return null; //todo
+        }
     }
 }
