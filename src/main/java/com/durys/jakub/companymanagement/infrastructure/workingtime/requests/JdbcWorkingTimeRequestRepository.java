bdccd839dc47251216998ab9c2a1655ead86c838
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-20 20:01:03 +0100

    init repository - workingtimerepository

---
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/requests/JdbcWorkingTimeRequestRepository.java
@@ -0,0 +1,23 @@
+package com.durys.jakub.companymanagement.infrastructure.workingtime.requests;
+
+import com.durys.jakub.companymanagement.domain.workingtime.requests.RequestInWorkflow;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestId;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@RequiredArgsConstructor
+public class JdbcWorkingTimeRequestRepository implements WorkingTimeRequestRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public RequestInWorkflow load(WorkingTimeRequestId id) {
+        return null;
+    }
+
+    @Override
+    public void save(RequestInWorkflow request) {
+
+    }
+}
