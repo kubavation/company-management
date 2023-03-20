0019e5443e585b9af39a917771d9ca324ff860cc
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-20 20:02:59 +0100

    bean for billingPeriodPolicy

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/ScheduleConfiguration.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/ScheduleConfiguration.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.companymanagement.infrastructure.workingtime;
 
 import com.durys.jakub.companymanagement.domain.workingtime.ScheduleRepository;
+import com.durys.jakub.companymanagement.domain.workingtime.billingperiod.BillingPeriodPolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,5 +12,10 @@ public class ScheduleConfiguration {
     @Bean
     ScheduleRepository scheduleRepository(JdbcTemplate jdbcTemplate) {
         return new JdbcScheduleRepository(jdbcTemplate);
+    }
+
+    @Bean
+    BillingPeriodPolicy billingPeriodPolicy() {
+        return new NoopBillingPeriodPolicy();
     }
 }
