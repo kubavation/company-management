18c69b2f22286fbc505084c072e9f1308f9bc38d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-20 20:13:23 +0100

    EmployeeFinder interface

---
+++ src/main/java/com/durys/jakub/companymanagement/readmodel/employees/EmployeeFinder.java
@@ -0,0 +1,7 @@
+package com.durys.jakub.companymanagement.readmodel.employees;
+
+import java.util.List;
+
+public interface EmployeeFinder {
+    List<EmployeeWithPersonalData> findEmployeesWithPersonalData();
+}
