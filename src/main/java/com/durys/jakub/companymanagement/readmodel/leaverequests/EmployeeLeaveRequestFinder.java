944424a382b245e52f72dd27bd1a1dd0620237ac
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-21 20:13:31 +0100

    added search criteria to employeeLeaveRequest

--- src/main/java/com/durys/jakub/companymanagement/readmodel/leaverequests/EmployeeLeaveRequestFinder.java
+++ src/main/java/com/durys/jakub/companymanagement/readmodel/leaverequests/EmployeeLeaveRequestFinder.java
@@ -1,9 +1,10 @@
 package com.durys.jakub.companymanagement.readmodel.leaverequests;
 
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
+import com.durys.jakub.companymanagement.readmodel.shared.SearchCriteria;
 
 import java.util.List;
 
 public interface EmployeeLeaveRequestFinder {
-    List<EmployeeLeaveRequest> findAll(EmployeeId employeeId);
+    List<EmployeeLeaveRequest> findAll(EmployeeId employeeId, SearchCriteria searchCriteria);
 }
--- src/main/java/com/durys/jakub/companymanagement/readmodel/leaverequests/impl/JpaEmployeeLeaveRequestFinder.java
+++ src/main/java/com/durys/jakub/companymanagement/readmodel/leaverequests/impl/JpaEmployeeLeaveRequestFinder.java
@@ -3,6 +3,7 @@ package com.durys.jakub.companymanagement.readmodel.leaverequests.impl;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRequest;
 import com.durys.jakub.companymanagement.readmodel.leaverequests.EmployeeLeaveRequestFinder;
+import com.durys.jakub.companymanagement.readmodel.shared.SearchCriteria;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -14,7 +15,7 @@ public class JpaEmployeeLeaveRequestFinder implements EmployeeLeaveRequestFinder
     private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public List<EmployeeLeaveRequest> findAll(EmployeeId employeeId) {
+    public List<EmployeeLeaveRequest> findAll(EmployeeId employeeId, SearchCriteria searchCriteria) {
         return null; //todo
     }
 }
