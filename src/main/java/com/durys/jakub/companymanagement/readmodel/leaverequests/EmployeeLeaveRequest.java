0a5ef8241d60e3175c99aaed3fd126222239701a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-21 20:09:58 +0100

    EmployeeLeaveRequest

--- src/main/java/com/durys/jakub/companymanagement/readmodel/leaverequests/EmployeeLeaveRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/readmodel/leaverequests/EmployeeLeaveRequest.java
@@ -1,4 +1,19 @@
 package com.durys.jakub.companymanagement.readmodel.leaverequests;
 
-public record EmployeeLeaveRequest(String leaveRequestId) {
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Builder
+public class EmployeeLeaveRequest {
+    private UUID id;
+    private UUID acceptantId;
+    private String acceptantName;
+    private String type;
+    private LocalDate from;
+    private LocalDate to;
+    private BigDecimal hours;
+    private BigDecimal minutes;
 }
