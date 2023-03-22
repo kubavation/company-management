f33128d1c6e1025f787d90d06419f524a8ce01c0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-22 18:34:56 +0100

    SubmittedWorkingTimeRequest implementation of RequestInWorkflow and WorkingTimeRequest

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SubmittedWorkingTimeRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SubmittedWorkingTimeRequest.java
@@ -1,9 +1,10 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
 import com.durys.jakub.companymanagement.domain.employees.model.Employee;
+import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.NonNull;
 
-public record SubmittedWorkingTimeRequest(WorkingTimeRequest request) implements RequestInWorkflow {
+public record SubmittedWorkingTimeRequest(WorkingTimeRequest request) implements RequestInWorkflow, WorkingTimeRequest {
 
     public SubmittedWorkingTimeRequest(@NonNull WorkingTimeRequest request) {
         this.request = request;
@@ -15,5 +16,20 @@ public record SubmittedWorkingTimeRequest(WorkingTimeRequest request) implements
 
     public CancelledWorkingTimeRequest cancel() {
         return new CancelledWorkingTimeRequest(request);
+    }
+
+    @Override
+    public WorkingTimeRequestId id() {
+        return request.id();
+    }
+
+    @Override
+    public EmployeeId authorId() {
+        return request.authorId();
+    }
+
+    @Override
+    public RequestInformation information() {
+        return request.information();
     }
 }
