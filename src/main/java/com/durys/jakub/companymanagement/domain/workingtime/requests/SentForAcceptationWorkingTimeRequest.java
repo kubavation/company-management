f0db1685ab9da77fce15ac4518d77b197479e708
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-22 18:40:44 +0100

    SentForAcceptationWorkingTimeRequest implementation of WorkingTimeRequest

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SentForAcceptationWorkingTimeRequest.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/SentForAcceptationWorkingTimeRequest.java
@@ -3,7 +3,7 @@ package com.durys.jakub.companymanagement.domain.workingtime.requests;
 import com.durys.jakub.companymanagement.domain.employees.model.EmployeeId;
 import lombok.NonNull;
 
-public class SentForAcceptationWorkingTimeRequest implements RequestInWorkflow {
+public class SentForAcceptationWorkingTimeRequest implements RequestInWorkflow, WorkingTimeRequest {
 
     private final WorkingTimeRequest request;
     private EmployeeId acceptantId;
@@ -30,4 +30,18 @@ public class SentForAcceptationWorkingTimeRequest implements RequestInWorkflow {
         return request;
     }
 
+    @Override
+    public WorkingTimeRequestId id() {
+        return request.id();
+    }
+
+    @Override
+    public EmployeeId authorId() {
+        return request.authorId();
+    }
+
+    @Override
+    public RequestInformation information() {
+        return request.information();
+    }
 }
