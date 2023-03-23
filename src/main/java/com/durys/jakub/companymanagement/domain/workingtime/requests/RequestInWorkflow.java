1e7a6309872e31c7c9f3305adbe73c90168d00f5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-23 20:35:40 +0100

    default author method in RequestInWorkflow

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/RequestInWorkflow.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/RequestInWorkflow.java
@@ -2,4 +2,7 @@ package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
 public interface RequestInWorkflow {
     WorkingTimeRequest request();
+    default Author author() {
+        return request().author();
+    }
 }
