d3b59a60cf4b281daddf87d872b444686ea342b3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-27 17:44:12 +0200

    removed unused dependencies in workingTimeService

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequestService.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/WorkingTimeRequestService.java
@@ -1,14 +1,9 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
 import com.durys.jakub.companymanagement.commons.domain.DomainService;
-import lombok.RequiredArgsConstructor;
 
 @DomainService
-@RequiredArgsConstructor
 public class WorkingTimeRequestService {
-
-    private final WorkingTimeRequestRepository requestRepository;
-
 
     public static SentForAcceptationWorkingTimeRequest asAcceptable(RequestInWorkflow request) {
 
