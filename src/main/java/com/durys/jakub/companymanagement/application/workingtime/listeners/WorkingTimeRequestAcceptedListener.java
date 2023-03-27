fdddfd23b6dee7e9ba9a4b2c0b671ab25e716647
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-27 20:13:55 +0200

    little refactor + import fix

--- src/main/java/com/durys/jakub/companymanagement/application/workingtime/listeners/WorkingTimeRequestAcceptedListener.java
+++ src/main/java/com/durys/jakub/companymanagement/application/workingtime/listeners/WorkingTimeRequestAcceptedListener.java
@@ -1,16 +1,14 @@
 package com.durys.jakub.companymanagement.application.workingtime.listeners;
 
 import com.durys.jakub.companymanagement.commons.events.DomainEventListener;
-import com.durys.jakub.companymanagement.domain.workingtime.*;
+import com.durys.jakub.companymanagement.domain.workingtime.Schedule;
+import com.durys.jakub.companymanagement.domain.workingtime.ScheduleRepository;
+import com.durys.jakub.companymanagement.domain.workingtime.ScheduleService;
 import com.durys.jakub.companymanagement.domain.workingtime.event.WorkingTimeRequestAcceptedEvent;
-import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 @Component
 @RequiredArgsConstructor
@@ -25,8 +23,8 @@ public class WorkingTimeRequestAcceptedListener {
 
         Schedule schedule = scheduleRepository.load(event.scheduleId());
 
-        var handler = scheduleService.handlerFrom(event);
-        handler.accept(schedule);
+        scheduleService.handlerFrom(event)
+                .accept(schedule);
 
         scheduleRepository.save(schedule);
     }
