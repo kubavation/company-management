566ee2baaaf8e40b2cd6bbd37ee24627322d8c76
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-27 19:43:21 +0200

    basic structure of onWorkingTimeRequestAcceptation - TODO

--- src/main/java/com/durys/jakub/companymanagement/application/workingtime/listeners/WorkingTimeRequestAcceptedListener.java
+++ src/main/java/com/durys/jakub/companymanagement/application/workingtime/listeners/WorkingTimeRequestAcceptedListener.java
@@ -1,7 +1,10 @@
 package com.durys.jakub.companymanagement.application.workingtime.listeners;
 
 import com.durys.jakub.companymanagement.commons.events.DomainEventListener;
+import com.durys.jakub.companymanagement.domain.workingtime.Schedule;
 import com.durys.jakub.companymanagement.domain.workingtime.ScheduleRepository;
+import com.durys.jakub.companymanagement.domain.workingtime.WorkDay;
+import com.durys.jakub.companymanagement.domain.workingtime.WorkDayEventPeriod;
 import com.durys.jakub.companymanagement.domain.workingtime.event.WorkingTimeRequestAcceptedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -17,6 +20,14 @@ public class WorkingTimeRequestAcceptedListener {
     @Transactional
     @DomainEventListener
     public void onWorkingTimeRequestAcceptation(WorkingTimeRequestAcceptedEvent event) {
+
+        Schedule schedule = scheduleRepository.load(event.scheduleId());
+
+        if (!(schedule instanceof WorkDay workDay)) {
+          throw new UnsupportedOperationException();
+        }
+
         //todo
+        workDay.assignPrivateExit(new WorkDayEventPeriod(event.period().from(), event.period().to()));
     }
 }
