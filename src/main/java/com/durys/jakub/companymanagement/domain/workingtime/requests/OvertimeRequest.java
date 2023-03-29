76d7cbd81f7cf7341709b1724db8ec42c507e18f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-29 19:13:12 +0200

    OvertimeRequest

---
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/OvertimeRequest.java
@@ -0,0 +1,32 @@
+package com.durys.jakub.companymanagement.domain.workingtime.requests;
+
+import com.durys.jakub.companymanagement.domain.workingtime.requests.vo.RequestInformation;
+import com.durys.jakub.companymanagement.domain.workingtime.requests.vo.WorkingTimeRequestId;
+
+public class OvertimeRequest implements WorkingTimeRequest {
+
+    private final WorkingTimeRequestId id;
+    private final RequestInformation information;
+    private final Author author;
+
+    public OvertimeRequest(WorkingTimeRequestId id, Author author, RequestInformation information) {
+        this.id = id;
+        this.author = author;
+        this.information = information;
+    }
+
+    @Override
+    public WorkingTimeRequestId id() {
+        return id;
+    }
+
+    @Override
+    public Author author() {
+        return author;
+    }
+
+    @Override
+    public RequestInformation information() {
+        return information;
+    }
+}
