bc4b0838176770febd602595bfba6d3ecbbbca14
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-03 20:26:41 +0200

    fix

--- src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/requests/HashMapWorkingTimeRequestRepository.java
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/requests/HashMapWorkingTimeRequestRepository.java
@@ -3,12 +3,15 @@ package com.durys.jakub.companymanagement.infrastructure.workingtime.requests;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.RequestInWorkflow;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.WorkingTimeRequestRepository;
 import com.durys.jakub.companymanagement.domain.workingtime.requests.vo.WorkingTimeRequestId;
+import com.durys.jakub.companymanagement.infrastructure.absences.leaverequests.output.LeaveRequestEntity;
 
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.UUID;
 
 public class HashMapWorkingTimeRequestRepository implements WorkingTimeRequestRepository {
 
-    private static final HashMap<WorkingTimeRequestId, RequestInWorkflow> DB = new HashMap<>();
+    private static final HashMap<UUID, LeaveRequestEntity> DB = new HashMap<>();
 
     @Override
     public RequestInWorkflow load(WorkingTimeRequestId id) {
