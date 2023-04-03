e56f664f022fb1109ea8c394b8d0a48e95e2cda0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-03 20:12:07 +0200

    WorkingTimeRequestStatus

---
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/requests/output/WorkingTimeRequestStatus.java
@@ -0,0 +1,10 @@
+package com.durys.jakub.companymanagement.infrastructure.workingtime.requests.output;
+
+public enum WorkingTimeRequestStatus {
+    SUBMITTED,
+    CANCELLED,
+    DELETED,
+    SENT_FOR_ACCEPTATION,
+    ACCEPTED,
+    REJECTED;
+}
