16072a30658c2ae2a14c556f0cbfa2e1d91607bc
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-03 18:58:44 +0200

    usage of lombok in vo

--- src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeavePrivilegesPeriod.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/absences/leaveprivileges/LeavePrivilegesPeriod.java
@@ -3,6 +3,7 @@ package com.durys.jakub.companymanagement.domain.absences.leaveprivileges;
 import com.durys.jakub.companymanagement.commons.domain.ValueObject;
 import com.durys.jakub.companymanagement.domain.absences.leaveprivileges.exception.InvalidLeavePrivilegesPeriodException;
 import lombok.Getter;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 import java.util.Objects;
@@ -14,10 +15,7 @@ class LeavePrivilegesPeriod {
     private final LocalDate dateFrom;
     private final LocalDate dateTo;
 
-    public LeavePrivilegesPeriod(LocalDate dateFrom, LocalDate dateTo) {
-
-        Objects.requireNonNull(dateFrom, "Date from value not provided");
-        Objects.requireNonNull(dateTo, "Date from value not provided");
+    public LeavePrivilegesPeriod(@NonNull LocalDate dateFrom,@NonNull LocalDate dateTo) {
 
         if (dateTo.isBefore(dateFrom)) {
             throw new InvalidLeavePrivilegesPeriodException();
