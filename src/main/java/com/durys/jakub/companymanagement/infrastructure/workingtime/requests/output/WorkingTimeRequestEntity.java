13e0d560c9f8775ada77b35f637bbf3c5d115222
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-03 20:09:19 +0200

    init of WorkingTimeRequestEntity

---
+++ src/main/java/com/durys/jakub/companymanagement/infrastructure/workingtime/requests/output/WorkingTimeRequestEntity.java
@@ -0,0 +1,31 @@
+package com.durys.jakub.companymanagement.infrastructure.workingtime.requests.output;
+
+import com.durys.jakub.companymanagement.infrastructure.employees.output.EmployeeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "CM_WORKING_TIME_REQUEST")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkingTimeRequestEntity {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private EmployeeEntity employee;
+
+    private LocalDate day;
+
+    private LocalTime from;
+    private LocalTime to;
+}
