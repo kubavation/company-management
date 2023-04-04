8387ee98b7c6ab1b6a706d5ec9df4aa67a5ecc24
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-04 18:45:47 +0200

    added sealed keyword to RequestInWorkflow interface

--- src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/RequestInWorkflow.java
+++ src/main/java/com/durys/jakub/companymanagement/domain/workingtime/requests/RequestInWorkflow.java
@@ -1,6 +1,9 @@
 package com.durys.jakub.companymanagement.domain.workingtime.requests;
 
-public interface RequestInWorkflow {
+public sealed interface RequestInWorkflow permits AcceptedWorkingTimeRequest,
+                    CancelledWorkingTimeRequest, RejectedWorkingTimeRequest,
+                    SentForAcceptationWorkingTimeRequest, SubmittedWorkingTimeRequest {
+
     WorkingTimeRequest request();
     default Author author() {
         return request().author();
